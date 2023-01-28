faa5b6b3567518fd5519a3e9bae5ea5288d69aa5
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-01-28 12:24:10 +0530

    Time: 75 ms (20.62%), Space: 89.4 MB (55.23%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -9,13 +9,18 @@ public:
         while(low<=high){
             int cut1 =(low+high) >> 1;
             int cut2 = (n1+n2+1)/2 - cut1;
-
+            
+            // l1 -> Left Partition of Array 1
+            // l2 -> Left Partition of Array 2
             int l1 = cut1==0 ? INT_MIN : nums1[cut1-1];
             int l2 = cut2==0 ? INT_MIN : nums2[cut2-1];
-
+                
+            // r1 -> Right Partition of Array 1
+            // r2 -> Right Partition of Array 2
             int r1 = cut1==n1 ? INT_MAX : nums1[cut1];
             int r2 = cut2==n2 ? INT_MAX : nums2[cut2];
 
+            // This Condition makes sure we have sorted elements in left part
             if(l1<=r2 && l2<=r1){
                 if((n1+n2)%2==0){
                     return (max(l1,l2)+min(r1,r2))/2.0;
