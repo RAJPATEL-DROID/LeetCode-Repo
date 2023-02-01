64ccd434d710466ba9e911ea8623e09cbd35306f
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-02-01 23:47:06 +0530

    Added solution - LeetHub

--- Isomorphic Strings - GFG/isomorphic-strings.cpp
+++ Isomorphic Strings - GFG/isomorphic-strings.cpp
@@ -16,23 +16,33 @@ class Solution
     bool areIsomorphic(string str1, string str2){
         
          if(str1.size()!=str2.size()){return false;}
-
-        int m1[256]={0}, m2[256]={0}, n=str1.size();
-
-        for(int i=0;i<n;i++)
-
-        {
-
-            if(m1[str1[i]]!=m2[str2[i]])
-
-                return false;
-
-            m1[str1[i]]=m2[str2[i]]=i+1;
-
-        }
-
-        return true;
+        // int m1[256]={0}, m2[256]={0}, n=str1.size();
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(m1[str1[i]]!=m2[str2[i]])
+        //         return false;
+        //     m1[str1[i]]=m2[str2[i]]=i+1;
+        // }
+        // return true;
         
+        //   if(str1.size()!=str2.size())
+
+        //  return 0;
+
+        unordered_map<char,char> mp1,mp2;
+        for(int i=0;i<str1.length();i++){
+            char pp = str1[i];
+            mp1[pp] = str2[i];
+            mp2[str2[i]] = str1[i];
+        } 
+        for(int i=0;i<str1.size();i++){
+            char pp = str1[i];
+            if(mp1[str1[i]]!=str2[i])
+              return false;
+            if(mp2[str2[i]]!=str1[i])
+              return false;
+        }
+        return true;
     }
 };
 
