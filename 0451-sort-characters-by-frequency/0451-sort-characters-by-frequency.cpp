d093b5e04973a8f4c36f53d51f7ceef0a00c7c66
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-02-03 22:10:12 +0530

    Time: 7 ms (97.53%), Space: 9.2 MB (15.07%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,26 +1,43 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        string ans = "";
-        unordered_map<char,int > m;
-        priority_queue<pair<int,char> > mheap;
+//         string ans = "";
+//         unordered_map<char,int > m;
+//         priority_queue<pair<int,char> > mheap;
         
-        for(int i=0;i< s.length(); i++){
-            m[s[i]]++;
+//         for(int i=0;i< s.length(); i++){
+//             m[s[i]]++;
+//         }
+        
+//         for(auto it = m.begin(); it != m.end(); it++){
+//             mheap.push({it->second, it-> first});
+//         }
+        
+//         while(!mheap.empty()){
+//             int freq = mheap.top().first;
+//             int c = mheap.top().second;
+//             for(int i= 1;i <= freq; i++){
+//                 ans +=  c;
+//             }
+//             mheap.pop();
+//         }
+//         return ans;
+        
+        unordered_map<char, int> freqOfChar;
+        for(char c : s) {
+            freqOfChar[c] ++;
         }
         
-        for(auto it = m.begin(); it != m.end(); it++){
-            mheap.push({it->second, it-> first});
+        map<int,string> Str ;
+        for(auto v: freqOfChar){
+            int n = v.second;
+            char c = v.first;
+            Str[n] += string(n,c);
         }
-        
-        while(!mheap.empty()){
-            int freq = mheap.top().first;
-            int c = mheap.top().second;
-            for(int i= 1;i <= freq; i++){
-                ans +=  c;
-            }
-            mheap.pop();
+        string res;
+        for(auto rit = Str.rbegin(); rit != Str.rend(); ++rit) {
+            res += rit->second;
         }
-        return ans;
+        return res;
     } 
 };
