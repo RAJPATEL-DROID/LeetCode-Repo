9bce1322d0066c4f3f7a2d242835992c42bfdb85
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-02-09 13:42:56 +0530

    Time: 8 ms (79.25%), Space: 7.7 MB (28.73%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,15 +9,33 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        // Using Hashmap : O(N) Space memory
-        unordered_map<ListNode*,int> m;
-         while(head != NULL){
-             m[head]++;
-             if(m[head] >1){
-                 return head;
-             }
-             head = head->next;
-         }
-         return NULL;
+        // Using Hashmap : O(N) Space Complexity
+//         unordered_map<ListNode*,int> m;
+//          while(head != NULL){
+//              m[head]++;
+//              if(m[head] >1){
+//                  return head;
+//              }
+//              head = head->next;
+//          }
+//          return NULL;
+        
+        // Using O(1) Space 
+        ListNode* fast = head;
+        ListNode* slow = head;
+        ListNode* entry = head;
+        
+        while(fast != NULL && fast->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+            
+            if(slow==fast){
+                while(slow!=entry){
+                    slow = slow->next;entry = entry->next;
+                }    
+                return entry;
+            }
+        }
+        return NULL;
     }
 };
