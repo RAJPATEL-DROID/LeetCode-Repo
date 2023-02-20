daa0550a8f8779c863d864e5cc8733175fa06bb1
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-02-20 21:03:02 +0530

    Added solution - LeetHub

--- Add 1 to a number represented as linked list - GFG/add-1-to-a-number-represented-as-linked-list.cpp
+++ Add 1 to a number represented as linked list - GFG/add-1-to-a-number-represented-as-linked-list.cpp
@@ -46,31 +46,73 @@ struct Node
 class Solution
 {
     public:
-    int addRecursive(Node* head){
-        // base case
-        if(head==NULL)return 1;
+    
+    
+    // RECURSIVE METHOD for ADDING 1
+    // int addRecursive(Node* head){
+    //     // base case
+    //     if(head==NULL)return 1;
         
-        int carry = addRecursive(head->next);
-        int curr = head->data + carry;
-        head->data = curr%10;
-        return (curr/10);
+    //     int carry = addRecursive(head->next);
+    //     int curr = head->data + carry;
+    //     head->data = curr%10;
+    //     return (curr/10);
+    // }
+
+    // Node* addOne(Node *head) 
+    // {
+    //     // Your Code here
+    //     // return head of list after adding one
+    //     int carry = addRecursive(head);
+        
+    //     if(carry){
+    //         Node* newNode = new Node(carry);
+    //         newNode->next = head;
+    //         head = newNode;
+    //     }
+    //     return head;
+        
+    // }
+
+
+    // Reverse the List and add 
+    Node* reverse(Node* head){
+        Node* prev = NULL;
+        Node* next = head->next;
+        Node* curr = head;
+        while(curr != NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev= curr;
+            curr = next;
+        }
+        return prev;
+    }
+    
+    Node* addOne(Node* head){
+        head = reverse(head);
+        Node* temp = head;
+        
+        while(temp != NULL){
+            if(temp->next == NULL && temp->data == 9){
+                temp->data = 1;
+                Node* newNode = new Node(0);
+                newNode->next = head;
+                head = newNode;
+                temp = temp->next;
+            }else if(temp->data == 9){
+                temp->data =0;
+                temp = temp->next;
+            }else{
+                temp->data = temp->data + 1;
+                break;
+            }
+        }
+        head = reverse(head);
+        return head;
     }
 
-    Node* addOne(Node *head) 
-    {
-        // Your Code here
-        // return head of list after adding one
-        int carry = addRecursive(head);
-        
-        if(carry){
-            Node* newNode = new Node(carry);
-            newNode->next = head;
-            head = newNode;
-        }
-        return head;
-        
-    }
-};
+};  
 
 //{ Driver Code Starts.
 
