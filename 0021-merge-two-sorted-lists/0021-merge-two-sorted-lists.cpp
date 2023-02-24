6fc962a96e5be269fe28be7bdebe1a6acd96f107
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-02-24 12:01:30 +0530

    Time: 8 ms (70.07%), Space: 14.8 MB (85.79%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,32 +11,57 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummy = new ListNode();
-        ListNode* last = dummy;
+        if(list1 == NULL)return list2;
+        if(list2 == NULL)return list1;
+        
+//         // Method 1 -> O(1) space complexity but more iterations
+//         ListNode* dummy = new ListNode();
+//         ListNode* last = dummy;
+        
+//         while(list1 != NULL && list2 != NULL){
+//                 if(list1->val >= list2->val){
+//                     last->next = list2;
+//                     last = last->next;
+//                     list2 = list2->next;
+//                 }else{
+//                     last->next = list1;
+//                     last = last->next;
+//                     list1 = list1->next;
+//                 }
+//         }
+        
+//         while(list1 !=NULL){
+//             last->next = list1;
+//             list1 = list1->next;
+//             last = last->next;
+//         }
+//         while(list2 != NULL){
+//             last->next = list2;
+//             last = last->next;
+//             list2 = list2->next;
+//         }
+        
+//         return dummy->next;
+        
+        // Method 2 -> O(1) Space Complexity but lesser iteration
+        if(list1->val > list2->val){
+            swap(list1,list2);
+        }
+        
+        ListNode* res = list1;
         
         while(list1 != NULL && list2 != NULL){
-                if(list1->val >= list2->val){
-                    last->next = list2;
-                    last = last->next;
-                    list2 = list2->next;
-                }else{
-                    last->next = list1;
-                    last = last->next;
-                    list1 = list1->next;
-                }
+            ListNode* temp = NULL;
+            while(list1 != NULL && list1->val <= list2->val){
+                temp = list1;
+                list1 = list1->next;
+            }
+            temp->next =list2;
+            // swap
+            swap(list1,list2);
         }
         
-        while(list1 !=NULL){
-            last->next = list1;
-            list1 = list1->next;
-            last = last->next;
-        }
-        while(list2 != NULL){
-            last->next = list2;
-            last = last->next;
-            list2 = list2->next;
-        }
-        
-        return dummy->next;
+        return res;
+            
     }
 };
