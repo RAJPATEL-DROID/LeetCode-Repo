0b12414760df2d6325b0274dbc5a34a14aea7518
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-03-07 09:52:27 +0530

    Time: 3 ms (75.82%), Space: 7.1 MB (63.71%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    int myAtoi(string str) {
-        int sign = 1, base = 0, i = 0;
-        while (str[i] == ' ') { i++; }   // skipping the whitespaces
-        if (str[i] == '-' || str[i] == '+') {
-            sign = 1 - 2 * (str[i++] == '-'); 
+     int f(string &str,int sign,int i,int result){
+        if(i >= str.size() || str[i] < '0' || str[i] > '9'){
+            return sign*result;
         }
-        while (str[i] >= '0' && str[i] <= '9') {
-            
-            // to avoid integer overflow
-            if (base >  INT_MAX / 10 || (base == INT_MAX / 10 && str[i] - '0' > 7)) {
+        int temp =0;
+        if(str[i] >= '0' or str[i] <= '9'){
+            temp = str[i] - '0';
+        } 
+        if (result >  INT_MAX / 10 || (result == INT_MAX / 10 && str[i] - '0' > 7)) {
                 if (sign == 1) return INT_MAX;
                 else return INT_MIN;
-            }
-            base  = 10 * base + (str[i++] - '0');
         }
-        return base * sign;
+        return f(str,sign,i+1,result*10+temp);
+     };
+
+    int myAtoi(string str) {
+           int sign = 1, base = 0, i = 0;
+           while (str[i] == ' ') { i++; }   // skipping the whitespaces
+           if (str[i] == '-' || str[i] == '+') {
+               sign = 1 - 2 * (str[i++] == '-'); 
+           }
+           int ans = f(str,sign,i,0);
+        return ans;
     }
 };
