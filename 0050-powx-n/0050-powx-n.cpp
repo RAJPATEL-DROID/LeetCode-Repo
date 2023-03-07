5a10aa6f72f9c785003104a27541c9f6cf9c4123
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-03-07 10:31:10 +0530

    Time: 0 ms (100.00%), Space: 6 MB (77.76%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
     double myPow(double x, int n){
-        if(n==0)return 1;   
+        // Recursive Way O(N) recursive stack space
+        // if(n==0)return 1;   
+        // if(n < 0){
+        //     n = abs(n);
+        //     x = 1/x;
+        // }
+        // if(n%2 ==0){
+        //     return myPow(x*x,n/2);
+        // }else{
+        //     return x*myPow(x,n-1);
+        // }
+        
+        // Iterative Way O(1) space
+        double num = 1;
+        long long nn = n;
+        if(nn < 0) nn = -nn;
+        while(nn>0){
+            if(nn%2==1){
+                num = num * x;
+                nn--;
+            }
+            else{
+                x = x*x;
+                nn/=2;
+            }
+        }
         if(n < 0){
-            n = abs(n);
-            x = 1/x;
+            num = 1.0/num;
         }
-        if(n%2 ==0){
-            return myPow(x*x,n/2);
-        }else{
-            return x*myPow(x,n-1);
-        }
+        return num;
     }
 };
