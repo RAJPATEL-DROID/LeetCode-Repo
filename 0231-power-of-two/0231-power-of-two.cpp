d8fd9012254f35f36fe2484fbdc741d8a595297f
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-03-27 00:40:21 +0530

    Time: 7 ms (12.28%), Space: 6 MB (15.00%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return  n > 0  && !( n & (n-1)) ;
+        return  n>0  && !( n & (n-1)) ;
         // int count =0;
         // if(n<=0)return false;
         // while(n){
