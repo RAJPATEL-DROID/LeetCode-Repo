51d6e0b4cab9c191c57827bb17f0e312d08887b1
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-04-01 10:05:20 +0530

    Time: 4 ms (37.83%), Space: 5.8 MB (92.96%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int count =0;
-        while(start > 0 && goal > 0){
-            if( (start&1) != (goal&1) )count++;
-            start >>= 1;
-            goal >>= 1;
-        }
         
-        while(start > 0){
-            if( start&1)count++;
-            start >>= 1;
+        // Brute Force/ Greedy Approach
+//         int count = 0;
+//         while(start > 0 && goal > 0){
+//             if( (start&1) != (goal&1) )count++;
+//             start >>= 1;
+//             goal >>= 1;
+//         }
+        
+//         while(start > 0){
+//             if( start&1)count++;
+//             start >>= 1;
+//         }
+//         while(goal > 0){
+//             if( goal&1 )count++;
+//             goal >>= 1;
+//         }
+//         return count;
+        
+        
+        // When We take XOR of two numbers,all bits which are different will be set in the result
+        // so a ^ b will have all the different bits set.
+        
+        int ans = start ^ goal;
+        
+        // now we will calculate the number of set bits using Brian Kernighan’s Algorithm
+        // x-1 will flip all the bits from rightmost set bit and all bits on the right of it
+        
+        int count =0;
+        
+        while(ans>0){
+            ans = ans & (ans-1);
+            count++;
         }
-        while(goal > 0){
-            if( goal&1 )count++;
-            goal >>= 1;
-        }
-        return count;
+        return count++;
     }
 };
