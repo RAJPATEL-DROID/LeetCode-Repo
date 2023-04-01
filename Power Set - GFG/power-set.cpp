13d9086892051df19dabb7043e26eef45fe7d552
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-04-01 12:48:51 +0530

    Added solution - LeetHub

--- Power Set - GFG/power-set.cpp
+++ Power Set - GFG/power-set.cpp
@@ -5,29 +5,51 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 	public:
-	    void generateString(string temp,vector<string>& ans,int ind,string s){
-	        if( ind == s.length()){
-	            if(temp == "")return;
-	            ans.push_back(temp);
-	            return;
-	        }
-	        
-	        generateString(temp+s[ind],ans,ind+1,s);
-	        generateString(temp,ans,ind+1,s);
-	        
-	    }
 	
-		vector<string> AllPossibleStrings(string s){
-		    //Code here
-		    if(s.length() == 1){
-		        return {s};
-		    }
-		    vector<string> ans;
-		    string temp = "";
-		    generateString(temp,ans,0,s);
-		    sort(ans.begin(),ans.end());
-		    return ans;
-		}
+	// Recursion : TC (2^N)
+// 	    void generateString(string temp,vector<string>& ans,int ind,string s){
+// 	        if( ind == s.length()){
+// 	            if(temp == "")return;
+// 	            ans.push_back(temp);
+// 	            return;
+// 	        }
+	        
+// 	        generateString(temp+s[ind],ans,ind+1,s);
+// 	        generateString(temp,ans,ind+1,s);
+	        
+// 	    }
+	
+// 		vector<string> AllPossibleStrings(string s){
+// 		    //Code here
+// 		    if(s.length() == 1){
+// 		        return {s};
+// 		    }
+// 		    vector<string> ans;
+// 		    string temp = "";
+// 		    generateString(temp,ans,0,s);
+// 		    sort(ans.begin(),ans.end());
+// 		    return ans;
+// 		}
+
+        // Bits Manipulation : TC(n*2^n)
+        
+        vector<string> AllPossibleStrings(string s){
+           vector<string> ans;
+           string temp;
+           
+           for(int i = 0; i <= pow(2,s.length())-1 ; i++){
+               temp = "";
+               for(int k=0 ; k < s.length(); k++){
+                   if( i  &  (1<<k)){
+                       temp += (s[k]);
+                   }
+               }
+               ans.push_back(temp);
+           }
+           sort(ans.begin(),ans.end());
+           ans.erase(ans.begin());
+           return ans;
+        }
 };
 
 //{ Driver Code Starts.
