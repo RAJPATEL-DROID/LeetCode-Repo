c8c2e005d02bda681e975ee1c244409876503121
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-04-05 16:50:06 +0530

    Added solution - LeetHub

--- Implement Queue using Linked List - GFG/implement-queue-using-linked-list.cpp
+++ Implement Queue using Linked List - GFG/implement-queue-using-linked-list.cpp
@@ -77,26 +77,31 @@ struct MyQueue {
 //Function to push an element into the queue.
 void MyQueue:: push(int x)
 {
-        // Your Code
-        QueueNode* node = new QueueNode(x);
-        if(rear == NULL){
-            front = node;
-            rear = node;
-        }else{
-            rear->next = node;
-            rear = node;
-        }
+    
+    QueueNode* temp = new QueueNode(x);
+    if(front == NULL && rear == NULL){
+        front = rear = temp;
+    }else{
+        rear->next  = temp;
+        rear = temp;
+    }
+    
 }
 
 //Function to pop front element from the queue.
 int MyQueue :: pop()
 {
         // Your Code  
-        if(front == NULL && rear == NULL){
-            return -1;
-        }
-        int temp = front->data;
-        front = front->next;
-        if(front == NULL)rear = NULL;
-        return temp;
+       if(front == NULL){
+           return -1;
+       }
+       int temp = front->data;
+       front = front->next;
+       if(front == NULL){
+           rear = NULL;
+       }
+       return temp;
+       
+       
+       
 }
