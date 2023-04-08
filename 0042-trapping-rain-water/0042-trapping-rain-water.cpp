479d848750ddec324d4df884a1f7c43c86e2905d
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-04-08 18:00:43 +0530

    Time: 28 ms (7.49%), Space: 20.4 MB (43.13%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,63 +1,29 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        // Using Stack
         stack<int> s;
-        vector<int> gr(height.size(),0);
-        vector<int> gl(height.size(),0);
-        
-        //gr
-        for(int i=height.size()-1; i >= 0; i--){
-            if(!s.empty() && s.top() > height[i]){
-                    if(i != height.size()-1 && s.top() < gr[i+1]){
-                        gr[i] = gr[i+1];
-                    }
-                    else{
-                        gr[i] = s.top();
-                    }
-                }else{
-                    while(!s.empty() && s.top() <= height[i]){
-                         s.pop();
-                    }           
-                    if(!s.empty()){
-                       if(i != height.size()-1 && s.top() < gr[i+1]){
-                            gr[i] = gr[i+1];
-                        }else{
-                            gr[i] = s.top();
-                        }
-                    }
-                }
-                s.push(height[i]);
+        int water =0;
+        int current =0;
+        while( current < height.size()){
+            while(!s.empty() && height[current] > height[s.top()]){
+                int top = s.top();
+                s.pop();
+                
+                if(s.empty())break;   // that is there are no block on left of current block
+                
+                
+                // we find the distance between curr element element in left
+                int distance = current- s.top()-1;
+                
+                // We find the minimum block from blocks of max height from left and right
+                // then we calculate how much water the container will carry between that
+                // block and current block
+                int bounded_height = min(height[current],height[s.top()]) - height[top];
+                water += bounded_height * distance;
             }
-        while(!s.empty()){
-            s.pop();
+            s.push(current++);
         }
-        // gl
-        for(int i=0; i< height.size(); i++){
-            if(!s.empty() && s.top() > height[i]){
-                if(i != 0 && s.top() < gl[i-1]){
-                    gl[i] = gl[i-1];
-                }
-                else  gl[i] = s.top();
-            }else{
-                while(!s.empty() && s.top() <= height[i]){
-                    s.pop();
-                }           
-                if(!s.empty()){
-                    if(i != 0 && s.top() < gl[i-1]){
-                        gl[i] = gl[i-1];
-                    }else{
-                        gl[i] = s.top();
-                    }
-                }
-            }
-            s.push(height[i]);
-        }
-        
-        int ans =0;
-        for(int i=0; i< height.size(); i++){
-            int qnt = min(gr[i],gl[i]);
-            ans += (qnt == 0)?0:(qnt-height[i]);
-        }
-        return ans;
+        return water;
     }
 };
