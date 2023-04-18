0cb12592aed2c7cc5eda8b768da8c946cc992035
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-04-18 10:33:03 +0530

    Time: 93 ms (82.63%), Space: 39.4 MB (97.79%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -10,11 +10,20 @@ public:
             while(!s.empty() && (i == arr.size() || arr[s.top()] >= arr[i])){
                 int mid = s.top();
                 s.pop();
+                
+                // Here we are finding the index of next smaller element and prev. smaller element 
+                // at a same time
+                
+                // index of prev smaller element
                 int leftBoundary = s.empty()?-1:s.top();
+                
+                // index of next smaller element
                 int rightBoundary = i;
                 
                 // count of subarray where mid is the minimum element
                 long count = (mid - leftBoundary)*(rightBoundary - mid) % mod;
+                
+                // add the total sum of all subarrays, min(b) where b was minimum element
                 z += (count * arr[mid])%mod;
                 z %= mod;
             }
