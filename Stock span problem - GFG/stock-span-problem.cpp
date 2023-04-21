d318a8f654e8f287d22d7a7d1efcf07b0a4c00c4
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-04-21 21:26:42 +0530

    Added solution - LeetHub

--- Stock span problem - GFG/stock-span-problem.cpp
+++ Stock span problem - GFG/stock-span-problem.cpp
@@ -13,28 +13,25 @@ class Solution
     vector <int> calculateSpan(int price[], int n)
     {
        // Your code here
-       vector<int> ans;
+       stack<pair<int,int> > s;
+       vector<int> ans(n,1);
+       for(int i=0; i< n; i++){
+          if(s.empty()){
+            ans[i] = -1;
+          }else{
+              while(!s.empty() && s.top().first <= price[i]){
+                  s.pop();
+              }
+              if(!s.empty())ans[i]  = s.top().second;
+              else ans[i] = -1;
+          }
+          s.push({price[i],i});
+       }
        
-       stack<pair<int,int> >s;
-       
-       for(int i= 0 ;i < n;i++){
-           if(s.empty()){
-              ans.push_back(-1);
-           }else if(!s.empty() && price[i] < s.top().first){
-                  ans.push_back(s.top().second);
-           }else if(!s.empty() && price[i] >= s.top().first){
-                  while(!s.empty() && price[i] >= s.top().first){
-                     s.pop();
-                  }
-                  if(!s.empty())ans.push_back(s.top().second);
-                  else ans.push_back(-1);
-            }
-            s.push({price[i],i});
-        }
-        for(int i=0; i< n;i++){
-            ans[i] = i - ans[i];
-        }
-        return ans;
+       for(int i=0; i< n; i++){
+           ans[i] = i- ans[i];
+       }
+       return ans;
     }
 };
 
