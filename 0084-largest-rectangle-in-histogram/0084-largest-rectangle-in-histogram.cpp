9922e5a1b4f69f23e834ccc1bfead3741309f68e
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-04-25 23:38:33 +0530

    Time: 155 ms (94.12%), Space: 75.3 MB (92.94%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,35 +1,56 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& h) {
-        int n = h.size();
-        vector<int> smlLeft(n),smlRight(n);
+
+        //        2 PASS  
+        // int n = h.size();
+//         vector<int> smlLeft(n),smlRight(n);
+//         stack<int> s;
+        
+//         for(int i= 0; i < n; i++){
+//             while(!s.empty() && h[s.top()] >= h[i] ){
+//                 s.pop();
+//             }
+//             if(s.empty())smlLeft[i] =0;
+//             else smlLeft[i] = s.top() + 1;
+//             s.push(i);
+//         }
+        
+//         while(!s.empty())s.pop();
+        
+        
+//         for(int i= n-1;i >=0 ;i--){
+//             while(!s.empty() && h[s.top()] >= h[i]){
+//                     s.pop();
+//             }
+//             if(s.empty())smlRight[i] = n-1;
+//             else smlRight[i] = s.top() - 1;
+//             s.push(i);
+//         }
+        
+//         int ans =0;
+//         for(int i=0;i <n;i++){
+//             ans = max(ans, h[i]* (smlRight[i] - smlLeft[i]+1 ));
+//         }
+//         return ans;
+        
+        // 1 PASS
+        int n = h.size();        
+         int ans =0;
         stack<int> s;
         
-        for(int i= 0; i < n; i++){
-            while(!s.empty() && h[s.top()] >= h[i] ){
+        for(int i =0 ; i <= n; i++){
+            while(!s.empty() && (i == n || h[s.top()] >= h[i]) ){
+                int height = h[s.top()];
                 s.pop();
+                int width;
+                if(s.empty())width = i;
+                else width = i- s.top()-1;
+                ans = max(ans,height * width);
             }
-            if(s.empty())smlLeft[i] =0;
-            else smlLeft[i] = s.top() + 1;
             s.push(i);
         }
         
-        while(!s.empty())s.pop();
-        
-        
-        for(int i= n-1;i >=0 ;i--){
-            while(!s.empty() && h[s.top()] >= h[i]){
-                    s.pop();
-            }
-            if(s.empty())smlRight[i] = n-1;
-            else smlRight[i] = s.top() - 1;
-            s.push(i);
-        }
-        
-        int ans =0;
-        for(int i=0;i <n;i++){
-            ans = max(ans, h[i]* (smlRight[i] - smlLeft[i]+1 ));
-        }
         return ans;
         
     }
