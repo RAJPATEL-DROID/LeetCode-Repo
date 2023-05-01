2e99f81f2f24f4dd04ff500deeea3956e142adb9
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-05-01 22:18:35 +0530

    Time: 4 ms (95.47%), Space: 8.2 MB (83.79%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-       unordered_map<char,int> m;
+       // unordered_map<char,int> m;
+       //  int i =0,j=0;
+       //  int n = s.length();
+       //  int len =0;
+       //  while(j < n){
+       //      if(m.find(s[j])  != m.end() && m[s[j]] >=i){
+       //          // cout<<"start"<<i << "end" << j<<'\n';
+       //          len = max(len,((j-1)-i+1));
+       //          // cout<<m[s[j]]<<"\n";
+       //          i = m[s[j]] + 1;
+       //          m.erase(s[j]);
+       //      }
+       //      m[s[j]] = j;
+       //      j++;
+       //  }
+       //  len = max(len,(j-1) - i +1);
+       //  return len;
+        
+        vector< int> m(256,-1);
         int i =0,j=0;
         int n = s.length();
         int len =0;
         while(j < n){
-            if(m.find(s[j])  != m.end() && m[s[j]] >=i){
-                // cout<<"start"<<i << "end" << j<<'\n';
-                len = max(len,((j-1)-i+1));
-                // cout<<m[s[j]]<<"\n";
-                i = m[s[j]] + 1;
-                m.erase(s[j]);
+            if(m[s[j]] != -1){
+                i = max(m[s[j]] +1 , i);
             }
+            
             m[s[j]] = j;
+            len = max(len, j - i +1);
             j++;
         }
-        len = max(len,(j-1) - i +1);
         return len;
         
     }
