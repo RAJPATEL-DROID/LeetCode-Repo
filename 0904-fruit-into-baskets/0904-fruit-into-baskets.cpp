539ead14c9383757ecf62c2fb73747e90ee49130
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-05-02 22:49:22 +0530

    Time: 195 ms (31.80%), Space: 71.1 MB (77.27%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        // Hash map 'basket' to store the types of fruits.
+        unordered_map<int, int> basket;
+        int left, right,cnt=0;
+        
+        // Add fruit from the right index (right) of the window.
+        for (left = 0, right = 0; right < fruits.size(); ++right) {
+            basket[fruits[right]]++;
+            
+            // If the current window has more than 2 types of fruit,
+            // we remove one fruit from the left index (left) of the window.
+            while(basket.size() > 2) {
+                basket[fruits[left]]--;
+                if (basket[fruits[left]] == 0)
+                    basket.erase(fruits[left]);
+                left++;
+            }
+            
+            cnt = max(cnt,right-left + 1);
+        }
+        
+        // Once we finish the iteration, the indexes left and right 
+        // stands for the longest valid subarray we encountered.
+        return cnt;
+    }
+};
