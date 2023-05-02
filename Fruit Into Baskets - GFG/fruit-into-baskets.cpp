620f2a075c4a77c29499301eaf39844df7b07ce9
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-05-02 22:50:24 +0530

    Added solution - LeetHub

--- Fruit Into Baskets - GFG/fruit-into-baskets.cpp
+++ Fruit Into Baskets - GFG/fruit-into-baskets.cpp
@@ -9,7 +9,7 @@ class Solution {
     int totalFruits(int N, vector<int> &fruits) {
          // Hash map 'basket' to store the types of fruits.
         unordered_map<int, int> basket;
-        int left, right;
+        int left, right,cnt=0;
         
         // Add fruit from the right index (right) of the window.
         for (left = 0, right = 0; right < fruits.size(); ++right) {
@@ -17,17 +17,19 @@ class Solution {
             
             // If the current window has more than 2 types of fruit,
             // we remove one fruit from the left index (left) of the window.
-            if (basket.size() > 2) {
+           while(basket.size() > 2) {
                 basket[fruits[left]]--;
                 if (basket[fruits[left]] == 0)
                     basket.erase(fruits[left]);
                 left++;
             }
+            
+            cnt = max(cnt,right-left+1);
         }
         
         // Once we finish the iteration, the indexes left and right 
         // stands for the longest valid subarray we encountered.
-        return right - left;
+        return cnt;
     }
 };
 
