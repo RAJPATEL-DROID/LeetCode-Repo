06eb9d5697e0c00f766fffebd1b44f6a3736ca99
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-05-03 22:25:48 +0530

    Time: 32 ms (84.23%), Space: 28.9 MB (75.74%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& A, int S) {
-        unordered_map<int, int> c({{0, 1}});
-        int psum = 0, res = 0;
-        for (int i : A) {
-            psum += i;
-            res += c[psum - S];
-            c[psum]++;
+    int atmost(vector<int>& A, int S){
+        if (S < 0) return 0;
+        int res = 0, i = 0, n = A.size();
+        for (int j = 0; j < n; j++) {
+            S -= A[j];
+            while (S < 0)
+                S += A[i++];
+            res += j - i + 1;
         }
         return res;
+    }; 
+    
+    int numSubarraysWithSum(vector<int>& A, int S) {
+        // unordered_map<int, int> c({{0, 1}});
+        // int psum = 0, res = 0;
+        // for (int i : A) {
+        //     psum += i;
+        //     res += c[psum - S];
+        //     c[psum]++;
+        // }
+        // return res;
+        
+        return atmost(A,S) - atmost(A,S-1);
     }
 };
