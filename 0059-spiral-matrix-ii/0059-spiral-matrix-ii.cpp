b149ffbab28c0b326a80fd1941c1e77ac38ac0e8
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-05-10 20:54:14 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (90.43%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> matrix(n,vector<int>(n));
+        if (n <= 0) return matrix;
+        
+        int num = 1, rowbegin = 0, rowend = n - 1, colbegin = 0, colend = n - 1;
+        while (rowbegin <= rowend && colbegin <= colend) {
+            for (int i = colbegin; i <= colend; i++) matrix[rowbegin][i] = num++;
+            rowbegin++;
+            
+            for (int i = rowbegin; i <= rowend; i++) matrix[i][colend] = num++;
+            colend--;
+            
+            for (int i = colend; i >= colbegin; i--) matrix[rowend][i] = num++;
+            rowend--;
+            
+            for (int i = rowend; i >= rowbegin; i--) matrix[i][colbegin] = num++;
+            colbegin++;
+        }
+        return matrix;
+    }
+};
