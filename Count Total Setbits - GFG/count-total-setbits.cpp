96efa09a5c3ed748d9108c1d7daa59616ee02c42
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-05-15 22:53:16 +0530

    Added solution - LeetHub

--- Count Total Setbits - GFG/count-total-setbits.cpp
+++ Count Total Setbits - GFG/count-total-setbits.cpp
@@ -7,13 +7,17 @@ using namespace std;
 
 class Solution {
   public:
-    long long countBits(long long N) {
+    long long countBits(long long n) {
         // code here
-        long long cnt =0;
-        for(long long i= 1;i <= N;i++){
-            cnt += __builtin_popcount(i);
-        }
-        return cnt;
+        // long long cnt =0;
+        // for(long long i= 1;i <= N;i++){
+        //     cnt += __builtin_popcount(i);
+        // }
+        // return cnt;
+        
+        if(n<=1)return n;
+        int x = log2(n);
+        return (x*pow(2,x-1) + (n-pow(2,x) + 1) + countBits(n - pow(2,x)) );
     }
 };
 
