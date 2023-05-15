b74fbac53f314809d86a19f6af305a99e62fb5c7
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-05-15 22:25:15 +0530

    Time: 702 ms (32.05%), Space: 180 MB (87.37%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -11,32 +11,44 @@
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-        ListNode* slow =head;
+//         ListNode* slow =head;
         
-        int count =0,value = 0;
-        while(slow != NULL){
-            if(count == k-1)value = slow->val;
-            slow = slow->next;
-            count++;
+//         int count =0,value = 0;
+//         while(slow != NULL){
+//             if(count == k-1)value = slow->val;
+//             slow = slow->next;
+//             count++;
+//         }
+        
+//         // cout<<count<<" "<<value<<" ";
+//         slow = head;
+//         int i=1;
+//         while(i != count+1 - k){
+//             slow = slow->next;
+//             i++;
+//         }
+//         i = slow->val;
+//         // cout<<i<<"\n";
+//         slow->val = value;
+//         slow = head;
+//         count =1;
+//         while(count != k){
+//             slow = slow->next;
+//             count++;
+//         }
+//         slow->val = i;
+//         return head;
+        
+         ListNode *n1 = nullptr, *n2 = nullptr;
+        for (auto p = head; p != nullptr; p = p->next) {
+            n2 = n2 == nullptr ? nullptr : n2->next;
+            if (--k == 0) {
+                n1 = p;
+                n2 = head;
+            }
         }
         
-        // cout<<count<<" "<<value<<" ";
-        slow = head;
-        int i=1;
-        while(i != count+1 - k){
-            slow = slow->next;
-            i++;
-        }
-        i = slow->val;
-        // cout<<i<<"\n";
-        slow->val = value;
-        slow = head;
-        count =1;
-        while(count != k){
-            slow = slow->next;
-            count++;
-        }
-        slow->val = i;
+        swap(n1->val,n2->val);
         return head;
     }
 };
