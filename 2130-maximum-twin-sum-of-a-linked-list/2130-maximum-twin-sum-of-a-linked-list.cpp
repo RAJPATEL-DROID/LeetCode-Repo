7e22151d0a2d61b6805c9c31d2a8e4d40fcce451
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-05-17 20:56:45 +0530

    Time: 263 ms (66.64%), Space: 116.7 MB (62.33%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -10,18 +10,53 @@
  */
 class Solution {
 public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL)return NULL;
+        ListNode* curr = head;
+        ListNode* frwd = curr->next;
+        ListNode* prev = NULL;
+        curr->next = prev;
+        
+        while(frwd!= NULL){
+            prev = curr;
+            curr = frwd;
+            frwd = curr->next;
+            curr->next = prev;
+        }
+        return curr;
+    }
     int pairSum(ListNode* head) {
-        vector<int> s;
+        // vector<int> s;
         ListNode* temp = head;
-        while(temp != NULL){
-            s.push_back(temp->val);
+        // while(temp != NULL){
+        //     s.push_back(temp->val);
+        //     temp = temp->next;
+        // }
+        // int ans =0;
+        // for(int i=0;i < s.size()/2;i++){
+        //     ans = max(ans,s[i] + s[s.size()-1-i]);
+        // }
+        // return ans;
+        
+        
+        ListNode* fast = head;
+        
+        while(fast->next->next && fast->next){
+            fast = fast->next->next;
             temp = temp->next;
         }
+        temp->next = reverseList(temp->next);
+        // cout<<temp->next->val;
+        fast = temp->next;
+        temp = head;
         int ans =0;
-        for(int i=0;i < s.size()/2;i++){
-            ans = max(ans,s[i] + s[s.size()-1-i]);
+        
+        while(fast){
+            ans = max(ans,temp->val+fast->val);
+            fast = fast->next;
+            temp = temp->next;
         }
-        return ans;
+        return ans;     
         
     }
 };
