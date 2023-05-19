7c819c320645000d7b93a7e0ad6024e3d6678ca6
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-05-19 21:43:10 +0530

    Time: 26 ms (60.12%), Space: 13.5 MB (61.58%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,27 +1,57 @@
 class Solution {
 public:
-     bool validColouring(vector<vector<int>>& gr, vector<int>& colour, int node, int col){
-        if(colour[node] != 0)
-            return (colour[node] == col);
+//      bool validColouring(vector<vector<int>>& gr, vector<int>& colour, int node, int col){
+//         if(colour[node] != 0)
+//             return (colour[node] == col);
 
-        colour[node] = col;
-        for(int ne : gr[node]){
-            if(!validColouring(gr, colour, ne, -col))
-                return false;
+//         colour[node] = col;
+//         for(int ne : gr[node]){
+//             if(!validColouring(gr, colour, ne, -col))
+//                 return false;
+//         }
+
+//         return true;
+//     }
+
+    bool isBipartite(vector<vector<int>>& graph) {
+//         int n = graph.size();
+//         vector<int> colour(n, 0);
+
+//         for(int node = 0; node < n; node++){
+//             if(colour[node]==0 && !validColouring(graph,colour, node, 1))
+//                 return false;
+//         }
+
+//         return true;
+        
+         int n = graph.size();
+        vector<int> colors(n, 0);
+        queue<int> q;
+        
+        for (int i = 0; i < n; i++) {
+            if (colors[i]) continue;
+            
+            colors[i] = 1;
+            q.push(i);
+            
+            while (!q.empty()) {
+                int temp = q.front();
+                
+                for (auto neighbor : graph[temp]) {
+                    
+					// Color neighbor with opposite color
+                    if (!colors[neighbor]){
+                        colors[neighbor] = -colors[temp];
+                        q.push(neighbor);
+                    }
+                    
+					// If the neighbor has the same color - can't bipartite.
+                    else if (colors[neighbor] == colors[temp]) 
+                        return false;
+                }
+                q.pop();
+            }
         }
-
-        return true;
-    }
-
-    bool isBipartite(vector<vector<int>>& gr) {
-        int n = gr.size();
-        vector<int> colour(n, 0);
-
-        for(int node = 0; node < n; node++){
-            if(colour[node]==0 && !validColouring(gr, colour, node, 1))
-                return false;
-        }
-
         return true;
     }
 };
