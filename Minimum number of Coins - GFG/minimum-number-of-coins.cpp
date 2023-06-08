bf5328869c69e6d2a46c5755ef1c2c535413c7c5
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-06-08 22:59:48 +0530

    Added solution - LeetHub

--- Minimum number of Coins - GFG/minimum-number-of-coins.cpp
+++ Minimum number of Coins - GFG/minimum-number-of-coins.cpp
@@ -49,11 +49,13 @@ public:
         // return ans;
         int coins[10]={1,2,5,10,20,50,100,200,500,2000};
         for(int i=9;i>=0;)
-        {if(coins[i]<=N)
-            { N-=coins[i];
-                ans.push_back(coins[i]);}
-            else
-            i--; }
+        {
+            if(coins[i]<=N){
+                N-=coins[i];
+                ans.push_back(coins[i]);
+            }else   i--; 
+            
+        }
         return ans; 
     }
 };
