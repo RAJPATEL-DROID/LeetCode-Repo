496a46b7a4c9b8d54c3c4d8d7a0dfa9d292183da
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-06-15 10:29:01 +0530

    Added solution - LeetHub

--- Minimum Platforms - GFG/minimum-platforms.cpp
+++ Minimum Platforms - GFG/minimum-platforms.cpp
@@ -13,20 +13,36 @@ class Solution{
     int findPlatform(int arr[], int dep[], int n)
     {
         // Your code here
-        sort(arr,arr+n);
-        sort(dep,dep+n);
-        int i=0, j=1;
-    	int platform = 1;
+    //     sort(arr,arr+n);
+    //     sort(dep,dep+n);
+    //     int i=0, j=1;
+    // 	int platform = 1;
     	
-    	while(i<n && j<n){
-    	    if( arr[j] > dep[i])
-    	        i++;
-    	    else
-    	        platform++;
+    // 	while(i<n && j<n){
+    // 	    if( arr[j] > dep[i])
+    // 	        i++;
+    // 	    else
+    // 	        platform++;
     	       
-    	    j++;
-    	}
-    	return platform;
+    // 	    j++;
+    // 	}
+    // 	return platform;
+        vector<pair<int,int>> vp;
+        for(int i=0;i < n;i++){
+            vp.push_back({arr[i],dep[i]});
+        }
+        
+        sort(vp.begin(),vp.end());
+        
+        priority_queue<int,vector<int> , greater<int> > pq;
+        
+        pq.push(vp[0].second);
+        for(int i=1;i < n;i++){
+            if(pq.top() < vp[i].first)pq.pop();
+            pq.push(vp[i].second);
+        }
+        
+        return pq.size();
     }
 };
 
