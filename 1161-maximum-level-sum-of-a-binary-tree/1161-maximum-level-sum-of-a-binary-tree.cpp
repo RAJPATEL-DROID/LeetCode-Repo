4563ef5355b7450b6eaee44b2d754f8d1dbc899a
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-06-15 18:05:54 +0530

    Time: 178 ms (98.35%), Space: 104.7 MB (95.47%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -9,28 +9,62 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int maxLevelSum(TreeNode* root) {
+//         int maxSum = INT_MIN;
+//         int ans =0;
+//         int level =0;
+//         queue<TreeNode*> q;
+//         q.push(root);
+        
+//         while(!q.empty()){
+//             level++;
+//             int sumAtCurrentLevel =0;
+            
+//             for(int i = q.size();i >0;i--){
+//                 TreeNode* node =q.front();
+//                 q.pop();
+//                 sumAtCurrentLevel += node->val;
+                
+//                 if(node->left != nullptr)q.push(node->left);
+//                 if(node->right != nullptr)q.push(node->right);
+//             }
+//             if(maxSum < sumAtCurrentLevel ){maxSum = sumAtCurrentLevel;ans = level;}
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
+    void dfs(TreeNode* node,int level,vector<int>& sumOfNodeAtLevel){
+            if(node == nullptr){
+                return;
+            }
+        
+            if(sumOfNodeAtLevel.size() == level){
+                sumOfNodeAtLevel.push_back(node->val);
+            }else{
+                sumOfNodeAtLevel[level] += node->val;
+            }
+        
+            dfs(node->left,level+1,sumOfNodeAtLevel);
+            dfs(node->right,level+1,sumOfNodeAtLevel);
+    }
+    
     int maxLevelSum(TreeNode* root) {
+        vector<int> sumOfNodeAtLevel;
+        dfs(root,0,sumOfNodeAtLevel);
+        
         int maxSum = INT_MIN;
         int ans =0;
-        int level =0;
-        queue<TreeNode*> q;
-        q.push(root);
         
-        while(!q.empty()){
-            level++;
-            int sumAtCurrentLevel =0;
-            
-            for(int i = q.size();i >0;i--){
-                TreeNode* node =q.front();
-                q.pop();
-                sumAtCurrentLevel += node->val;
-                
-                if(node->left != nullptr)q.push(node->left);
-                if(node->right != nullptr)q.push(node->right);
+        for(int i=0;i < sumOfNodeAtLevel.size();i++){
+            if(maxSum < sumOfNodeAtLevel[i]){
+                maxSum  = sumOfNodeAtLevel[i];
+                ans = i+1;
             }
-            if(maxSum < sumAtCurrentLevel ){maxSum = sumAtCurrentLevel;ans = level;}
         }
         return ans;
     }
