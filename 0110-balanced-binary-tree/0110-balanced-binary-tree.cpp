b2f20fedfc6a33ae68c48225f58c0ba631caef63
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-06-19 11:43:20 +0530

    Time: 17 ms (41.63%), Space: 20.8 MB (93.12%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -12,18 +12,32 @@
 class Solution {
 public:
     
-    int height(TreeNode* root){
-        if(root == nullptr)return 0;
+//     int height(TreeNode* root){
+//         if(root == nullptr)return 0;
         
-        int left = height(root->left);
-        int right = height(root->right);
-        return 1 + max(left,right);
+//         int left = height(root->left);
+//         int right = height(root->right);
+//         return 1 + max(left,right);
+//     }
+    
+    int dfs(TreeNode* root){
+        if(root == nullptr)return 1;
+        
+        int l = dfs(root->left);
+        int r = dfs(root->right);
+        
+        if(l == -1 || r == -1 || (abs(l-r) > 1)){
+            return -1;
+        }
+        return 1 + max(l,r);
     }
     bool isBalanced(TreeNode* root) {
-        if(root == nullptr)return 1;
-        int left= height(root->left);
-        int right = height(root->right);
+//         if(root == nullptr)return 1;
+//         int left= height(root->left);
+//         int right = height(root->right);
         
-        return (abs(left-right) <= 1) && isBalanced(root->left) && isBalanced(root->right);
+//         return (abs(left-right) <= 1) && isBalanced(root->left) && isBalanced(root->right);
+        
+        return dfs(root) != -1;
     }
 };
