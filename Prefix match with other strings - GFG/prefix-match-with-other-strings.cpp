6a4145265cb4584df306cbc28c5ba3dd698f5832
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-06-24 08:24:20 +0530

    Added solution - LeetHub

--- Prefix match with other strings - GFG/prefix-match-with-other-strings.cpp
+++ Prefix match with other strings - GFG/prefix-match-with-other-strings.cpp
@@ -9,26 +9,57 @@ using namespace std;
 
 class Solution{   
 public:
-    int klengthpref(string arr[], int n, int k, string str){    
-        int count =0;
+struct Node{
+        int pre_cnt;
+        unordered_map<char,Node*>next;
+        Node(){
+            pre_cnt=0;
+        }
+    };
+    int klengthpref(string arr[], int n, int k, string s){    
         
-        // Traverse through Array 
-        for(int i=0; i < n; i++){
-            int j=0;
-            // for each element of string check if the prefixes matches
-            for(j=0;j< k;j++){
-                if(str[j] != arr[i][j]){
-                    break;
-                }
-            }    
-            if(j == k){
-                // cout<<arr[i]<<"\n";
-                count++;
+        // BRUTE FORCE - TC O(N*K)
+        // int count =0;
+        
+        // // Traverse through Array 
+        // for(int i=0; i < n; i++){
+        //     int j=0;
+        //     // for each element of string check if the prefixes matches
+        //     for(j=0;j< k;j++){
+        //         if(str[j] != arr[i][j]){
+        //             break;
+        //         }
+        //     }    
+        //     if(j == k){
+        //         // cout<<arr[i]<<"\n";
+        //         count++;
                 
+        //     }
+        // }
+        // return count;
+        
+        // Using TRIE  TC : O(N*L)
+        
+        if(s.length() < k)return 0;
+        
+        Node* root = new Node();
+        int i,j;
+        for(int i=0; i < n; i++){
+            string x = arr[i];
+            Node* temp = root;
+            for(int j=0; j < x.length(); j++){
+                if(temp->next.find(x[j]) == temp->next.end())temp->next[x[j]] = new Node();
+                temp = temp->next[x[j]];
+                temp->pre_cnt++;
             }
         }
         
-        return count;
+        Node* temp =root;
+        for(i=0;i < k ; i++){
+            if(temp->next.find(s[i]) == temp->next.end())return 0;
+            else temp = temp->next[s[i]];
+        }
+        return temp->pre_cnt;
     }
 };
 
