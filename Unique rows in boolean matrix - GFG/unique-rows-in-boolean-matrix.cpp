5a0ef6b57a3bc8f1ecfbcf3d806d4e9ec3a96283
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-06-25 14:06:13 +0530

    Added solution - LeetHub

--- Unique rows in boolean matrix - GFG/unique-rows-in-boolean-matrix.cpp
+++ Unique rows in boolean matrix - GFG/unique-rows-in-boolean-matrix.cpp
@@ -11,22 +11,39 @@ class Solution
 {
     public:
     // #define MAX 1000
-    vector<vector<int>> uniqueRow(int M[MAX][MAX],int row,int col)
+    vector<vector<int>> uniqueRow(int a[MAX][MAX],int r,int c)
     {
         //Your code here
-        set<vector<int>> s;
-        vector<vector<int> >ans;
         
-        for(int i=0;i< row;i++){
-            vector<int> temp;
-            for(int j=0;j<col;j++){
-                temp.push_back(M[i][j]);
+        // BRUTE_FOCRCE SC(R*C)
+        // set<vector<int>> s;
+        // vector<vector<int> >ans;
+        
+        // for(int i=0;i< row;i++){
+        //     vector<int> temp;
+        //     for(int j=0;j<col;j++){
+        //         temp.push_back(M[i][j]);
+        //     }
+        //     if(s.find(temp) == s.end())ans.push_back(temp);
+        //     s.insert(temp);
+        // }
+        
+        
+        // return ans;
+        
+        // Space Optimization SC(R)
+        vector<vector<int>>ans;
+        unordered_map<long long,int>mp;
+        for(int i=0;i<r;i++){
+            long long int binaryToDecimal=0;
+            vector<int>v;
+            for(int j=0;j<c;j++){
+                binaryToDecimal=2*binaryToDecimal+a[i][j];
+                v.push_back(a[i][j]);
             }
-            if(s.find(temp) == s.end())ans.push_back(temp);
-            s.insert(temp);
+            if(mp[binaryToDecimal]==0)ans.push_back(v);
+            mp[binaryToDecimal]++;
         }
-        
-        
         return ans;
     }
 };
