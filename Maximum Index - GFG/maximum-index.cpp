104d429a12f861e561a39b1ca39994890b185579
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-07-03 14:56:52 +0530

    Added solution - LeetHub

--- Maximum Index - GFG/maximum-index.cpp
+++ Maximum Index - GFG/maximum-index.cpp
@@ -8,19 +8,50 @@ class Solution{
 public:
     int maxIndexDiff(int arr[], int n) {
         // code here
-        vector<int>v;
-        int maxi=INT_MIN;
-        for(int i = n-1 ; i >=0 ; i--){
-            maxi=max(maxi,arr[i]);
-            v.push_back(maxi);
+        
+        // Binary Search 
+        
+        // vector<int>v;
+        // int maxi=INT_MIN;
+        // for(int i = n-1 ; i >=0 ; i--){
+        //     maxi=max(maxi,arr[i]);
+        //     v.push_back(maxi);
+        // }
+        // maxi=INT_MIN;
+        // for(int i = 0 ; i < n ; i++){
+        //     int idx=lower_bound(v.begin(),v.end(),arr[i])-v.begin();
+        //     idx=n-idx-1;
+        //     maxi=max(maxi,idx-i);
+        // }
+        // return maxi;
+        
+        // Optimal Solution
+        // Use 2 Arryas : Max Array and MinArray
+        vector<int> maxArr(n);
+        vector<int> minArr(n);
+        int maxi=INT_MIN,mini=INT_MAX;
+        for(int i=n-1; i >= 0; i--){
+             maxi = max(maxi,arr[i]);   
+             maxArr[i] = maxi;
         }
-        maxi=INT_MIN;
-        for(int i = 0 ; i < n ; i++){
-            int idx=lower_bound(v.begin(),v.end(),arr[i])-v.begin();
-            idx=n-idx-1;
-            maxi=max(maxi,idx-i);
+        
+        for(int i=0;i<n;i++){
+            mini = min(mini,arr[i]);
+            minArr[i]  = mini;
         }
-        return maxi;
+        
+        int ans = INT_MIN;
+        int i=0,j=0;
+        while(i < n && j < n){
+            if(minArr[i] <= maxArr[j]){
+                ans = max(ans,j-i);
+                j++;
+            }else {
+                i++;
+            }
+        }
+        return ans;
+        
     }
 
 };
