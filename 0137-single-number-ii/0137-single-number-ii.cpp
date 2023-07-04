d29dab3562dabde373ce4fd61a087277713b0113
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-07-04 19:26:04 +0530

    Time: 7 ms (82.86%), Space: 9.6 MB (41.15%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int result = 0;
+//         int result = 0;
 
-        int x, sum;
-        int n = nums.size();
-        // Iterate through every bit
-        for (int i = 0; i < 32; i++)
-        {
-          // Find sum of set bits at ith position in all
-          // array elements
-          sum = 0;
-          x = (1 << i);
-          for (int j=0; j< n; j++ )
-          {
-              if (nums[j] & x)
-                sum++;
-          }
+//         int x, sum;
+//         int n = nums.size();
+//         // Iterate through every bit
+//         for (int i = 0; i < 32; i++)
+//         {
+//           // Find sum of set bits at ith position in all
+//           // array elements
+//           sum = 0;
+//           x = (1 << i);
+//           for (int j=0; j< n; j++ )
+//           {
+//               if (nums[j] & x)
+//                 sum++;
+//           }
 
-          // The bits with sum not multiple of 3, are the
-          // bits of element with single occurrence.
-          if (sum % 3)
-            result |= x;
+//           // The bits with sum not multiple of 3, are the
+//           // bits of element with single occurrence.
+//           if (sum % 3)
+//             result |= x;
+//         }
+
+//         return result;
+        
+        int x1 = 0, x2 = 0, mask = 0;
+         
+        for (int i : nums) {
+            x2 ^= x1 & i;
+            x1 ^= i;
+            mask = ~(x1 & x2);
+            x2 &= mask;
+            x1 &= mask;
         }
 
-        return result;
+        return x1;
     }
 };
