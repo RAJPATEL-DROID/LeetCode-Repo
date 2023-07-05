d761e76ea647a94b13dfcd3118c42adc2ac05fc0
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-07-05 22:02:18 +0530

    Time: 106 ms (49.10%), Space: 56.9 MB (60.11%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -11,36 +11,55 @@
  */
 class Solution {
 public:
-    TreeNode* search(TreeNode* root,int val){
-        TreeNode* temp = root;
-        while(temp){
-            if(temp->val == val)return temp;
+//     TreeNode* search(TreeNode* root,int val){
+//         TreeNode* temp = root;
+//         while(temp){
+//             if(temp->val == val)return temp;
             
-            if(temp->val > val){
-                if(temp->left)temp = temp->left;
-                else break;
-            }else{
-                if(temp->right)temp =temp->right;
-                else break;
-            }
-        }
-        return temp;
-    }
+//             if(temp->val > val){
+//                 if(temp->left)temp = temp->left;
+//                 else break;
+//             }else{
+//                 if(temp->right)temp =temp->right;
+//                 else break;
+//             }
+//         }
+//         return temp;
+//     }
     
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if(root == NULL){
-            TreeNode* temp = new TreeNode(val);
-            root = temp;
-            return root;
-        }
-        TreeNode* res = search(root,val);
-        if(res->val != val){
-            TreeNode* temp = new TreeNode(val);
-            if(res->val > val){
-                res->left = temp;
-            }else
-            {
-                res->right = temp;
+        if(!root) return new TreeNode(val);
+        
+        // TreeNode* res = search(root,val);
+        // if(res->val != val){
+        //     TreeNode* temp = new TreeNode(val);
+        //     if(res->val > val){
+        //         res->left = temp;
+        //     }else
+        //     {
+        //         res->right = temp;
+        //     }
+        // }
+        // return root;
+        
+        
+        
+        auto curr = root;
+        
+        while(true){
+            if(curr->val < val){
+                if(curr->right) curr = curr->right;
+                else {
+                    curr->right = new TreeNode(val);
+                    break;
+                }
+            }
+            else{
+                if(curr->left) curr = curr->left;
+                else{
+                    curr->left = new TreeNode(val);
+                    break;
+                }
             }
         }
         return root;
