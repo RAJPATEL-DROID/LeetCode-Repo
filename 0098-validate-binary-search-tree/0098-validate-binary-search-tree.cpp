ba0a2f4aadfd407b342abf902be4547ee4231f3c
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-07-08 10:29:56 +0530

    Time: 26 ms (5.40%), Space: 21.8 MB (30.57%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,15 +11,28 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode* root,long mini,long maxi){
-        if(root == NULL)return true;
-        if(root->val >= maxi || root->val <= mini)return false;
+//     bool solve(TreeNode* root,long mini,long maxi){
+//         if(root == NULL)return true;
+//         if(root->val >= maxi || root->val <= mini)return false;
         
-        return solve(root->left,mini,root->val) && solve(root->right,root->val,maxi);
-    }
+//         return solve(root->left,mini,root->val) && solve(root->right,root->val,maxi);
+//     }
+//     bool isValidBST(TreeNode* root) {
+//         long mini = LONG_MIN;
+//         long maxi = LONG_MAX;
+//         return solve(root,mini,maxi);
+//     }
+    
+    // Use Prev Pointer to check condition,traversing in Inorder.
     bool isValidBST(TreeNode* root) {
-        long mini = LONG_MIN;
-        long maxi = LONG_MAX;
-        return solve(root,mini,maxi);
+        TreeNode* prev = NULL;
+        return validate(root, prev);
+    }
+    bool validate(TreeNode* node, TreeNode* &prev) {
+        if (node == NULL) return true;
+        if (!validate(node->left, prev)) return false;
+        if (prev != NULL && prev->val >= node->val) return false;
+        prev = node;
+        return validate(node->right, prev);
     }
 };
