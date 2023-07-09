165a0df0d9f0661e7b80b5c38d0d0f82d598c18c
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-07-09 12:09:37 +0530

    Added solution - LeetHub

--- Smallest Positive missing number - GFG/smallest-positive-missing-number.cpp
+++ Smallest Positive missing number - GFG/smallest-positive-missing-number.cpp
@@ -12,20 +12,63 @@ class Solution
     int missingNumber(int arr[], int n) 
     { 
         // Your code here
-        if(n==1 && arr[0]>1)return 1;
-        else if(n==1 && arr[0] == 1)return arr[0]+1;
+        // BRUTE FORCE
+        // if(n==1 && arr[0]>1)return 1;
+        // else if(n==1 && arr[0] == 1)return arr[0]+1;
         
-        sort(arr,arr+n);
-        for(int i=0;i < n-1;i++){
-            if(arr[i]>0){
-                if(arr[i-1] <= 0 && arr[i] != 1)return 1;
-                if(arr[i+1]-arr[i] > 1)return arr[i]+1;
-                else continue;
-            }else continue;
+        // sort(arr,arr+n);
+        // for(int i=0;i < n-1;i++){
+        //     if(arr[i]>0){
+        //         if(arr[i-1] <= 0 && arr[i] != 1)return 1;
+        //         if(arr[i+1]-arr[i] > 1)return arr[i]+1;
+        //         else continue;
+        //     }else continue;
+        // }
+        
+        // if(arr[n-1]<0)return 1;
+        // if(arr[n-2]>=0 && arr[n-1]-arr[n-2] ==1)return arr[n-1]+1;
+        // else return 1;
+        
+        /* Using Sorting 
+        O(NLogN) => time
+        O(N) => space
+        int cnt = 1;
+        
+        sort(arr, arr+n);
+        
+        for(int i=0; i<n; i++)
+        {
+            if(cnt == arr[i] && arr[i] <= n+1) cnt++;
+            
+            if(cnt > arr[i]) {
+                continue;
+            }
+            
+            if(arr[i] > cnt){
+                return cnt;
+            }
         }
-        if(arr[n-1]<0)return 1;
-        if(arr[n-2]>=0 && arr[n-1]-arr[n-2] ==1)return arr[n-1]+1;
-        else return 1;
+        
+        return cnt;
+        */
+        
+        // O(N)
+        
+        for(int i=0; i< n;i++){
+            if(arr[i] > 0 && arr[i] < n+1){
+                if(arr[arr[i]-1] != arr[i]){
+                    swap(arr[i],arr[arr[i]-1]);
+                    i--;
+                }
+            }
+        }
+        
+        for(int i=0; i < n;i++){
+            if(arr[i] != i+1)return i+1;
+        }
+        
+        return n+1;
+        
     } 
 };
 
