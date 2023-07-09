8254677bdbbe6cae1faa27587e8103d782ab2f23
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-07-09 12:11:03 +0530

    Added solution - LeetHub

--- Smallest Positive missing number - GFG/smallest-positive-missing-number.cpp
+++ Smallest Positive missing number - GFG/smallest-positive-missing-number.cpp
@@ -53,7 +53,30 @@ class Solution
         */
         
         // O(N)
-        
+        /*
+        Approach:
+    1. Since we have to find the smallest missing +ve element and the size is n
+    2. missing number should lie in domain [1,n+1].
+
+ 
+
+    In first loop:
+   1. whenever we encounter a +ve number we check that if number is in domain [1, n]. 
+    2. if number is in domain, we place it at its correct position (if the current number is duplicate then we don't place it and move on).
+    3. we swap the number with number at index number-1
+
+ 
+
+     At last, number n must be present at index n-1, n > 0
+
+ 
+
+    In second loop, we check if the number at index i doesn't equal to i+1, we return i+1 as it is the missing number.
+
+ 
+
+    if we don't find number in second loop then it means that all numbers in domain [1, n] are present.
+        */
         for(int i=0; i< n;i++){
             if(arr[i] > 0 && arr[i] < n+1){
                 if(arr[arr[i]-1] != arr[i]){
