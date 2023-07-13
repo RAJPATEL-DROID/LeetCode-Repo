81b03a91d0935e60c349fc74af051cb488369dab
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-07-13 09:14:59 +0530

    Added solution - LeetHub

--- Largest BST - GFG/largest-bst.cpp
+++ Largest BST - GFG/largest-bst.cpp
@@ -98,41 +98,57 @@ struct Node {
         left = right = NULL;
     }
 };*/
-class NodeValue {
-    public:
-        int maxNode,minNode,maxSize;
+// class NodeValue {
+//     public:
+//         int maxNode,minNode,maxSize;
         
-        NodeValue(int minNode,int maxNode,int maxSize){
-            this->minNode = minNode;
-            this->maxNode = maxNode;
-            this->maxSize = maxSize;
-        }   
-};
+//         NodeValue(int minNode,int maxNode,int maxSize){
+//             this->minNode = minNode;
+//             this->maxNode = maxNode;
+//             this->maxSize = maxSize;
+//         }   
+// };
 class Solution{
     private:
-    NodeValue helper(Node* root){
-        if(!root)return NodeValue(INT_MAX,INT_MIN,0);
+    // NodeValue helper(Node* root){
+    //     if(!root)return NodeValue(INT_MAX,INT_MIN,0);
     
     
-        auto left = helper(root->left);
-        auto right= helper(root->right);
+    //     auto left = helper(root->left);
+    //     auto right= helper(root->right);
         
-        if(left.maxNode < root->data && root->data < right.minNode){
-            return NodeValue(min(root->data,left.minNode),max(root->data,right.maxNode),
-            left.maxSize + right.maxSize + 1);
+    //     if(left.maxNode < root->data && root->data < right.minNode){
+    //         return NodeValue(min(root->data,left.minNode),max(root->data,right.maxNode),
+    //         left.maxSize + right.maxSize + 1);
+    //     }
+        
+    //     return NodeValue(INT_MIN,INT_MAX,max(left.maxSize,right.maxSize));
+    // }
+    
+    vector<int> helper(Node* root){
+        if(!root)return {INT_MAX,INT_MIN,0};
+        
+        vector<int> left = helper(root->left);
+        vector<int> right = helper(root->right);
+        
+        if(left[1] < root->data && root->data < right[0]){
+            return {min(root->data,left[0]),max(root->data,right[1]),left[2] + right[2] + 1};
+        }else{
+            return {INT_MIN,INT_MAX,max(left[2],right[2])};
         }
-        
-        return NodeValue(INT_MIN,INT_MAX,max(left.maxSize,right.maxSize));
     }
     public:
     /*You are required to complete this method */
     // Return the size of the largest sub-tree which is also a BST
+    
     int largestBst(Node *root)
     {
     	//Your code here
     	
-    	return helper(root).maxSize;
-    	
+    // 	return helper(root).maxSize;
+    
+        vector<int> res = helper(root);
+        return res[2];
     }
 };
 
