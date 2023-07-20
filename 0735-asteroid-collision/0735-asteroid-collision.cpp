93acf53c7f447caf13979e35de1371ba0ddc9f70
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-07-20 08:52:30 +0530

    Time: 11 ms (92.72%), Space: 17.7 MB (55.66%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,37 +1,72 @@
 class Solution {
 public:
+    // vector<int> ans;
+//         stack<int> s;
+//         int n = ast.size();
+//         for(int i=0; i< n; i++){
+//             if(ast[i] > 0){
+//                 s.push(ast[i]);
+//             }else{
+//                     int flag=1;
+//                     while(!s.empty() && (abs(ast[i]) - s.top()) >=0){
+//                         if(abs(ast[i])  == s.top()){
+//                             flag =0;
+//                             s.pop();
+//                             break;
+//                         }
+//                         s.pop();
+//                     }
+//                     if(s.empty() && flag){
+//                         ans.push_back(ast[i]);
+//                     }
+//                 }
+//             }
+//         vector<int> tmp;
+//         while(!s.empty())
+//         {
+//             tmp.push_back( s.top() );
+//             s.pop();
+//         }
+        
+//         for(int i= tmp.size()-1 ; i >= 0; i--){
+//             if(tmp[i] != 0)ans.push_back(tmp[i]);
+//         }
+//         return ans;
     vector<int> asteroidCollision(vector<int>& ast) {
+//         
         vector<int> ans;
         stack<int> s;
         int n = ast.size();
-        for(int i=0; i< n; i++){
+        for(int i=0; i < n;i++){
             if(ast[i] > 0){
                 s.push(ast[i]);
             }else{
-                    int flag=1;
-                    while(!s.empty() && (abs(ast[i]) - s.top()) >=0){
-                        if(abs(ast[i])  == s.top()){
-                            flag =0;
-                            s.pop();
-                            break;
-                        }
+                int flag =1;
+                while(!s.empty() && abs(ast[i]) - s.top() >= 0){
+                    if(abs(ast[i]) - s.top() == 0){
+                        flag =0;
                         s.pop();
+                        break;
                     }
-                    if(s.empty() && flag){
-                        ans.push_back(ast[i]);
-                    }
+                    s.pop();
                 }
-            }
-        vector<int> tmp;
-        while(!s.empty())
-        {
-            tmp.push_back( s.top() );
+                // If all the previous ast moving right are destroyed,
+                // then push our curr ast in stack
+                if(s.empty() && flag){
+                    ans.push_back(ast[i]);
+                }
+            } 
+        }
+        vector<int> temp;
+        while(!s.empty()){
+            temp.push_back(s.top());
             s.pop();
         }
         
-        for(int i= tmp.size()-1 ; i >= 0; i--){
-            if(tmp[i] != 0)ans.push_back(tmp[i]);
+        for(int i= temp.size() - 1 ; i >= 0; i--){
+            ans.push_back(temp[i]);
         }
         return ans;
+       
     }
 };
