cdbc97f39308753f2d6959e1b1059f48ebb67da1
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-07-25 09:19:39 +0530

    Time: 110 ms (95.46%), Space: 59.5 MB (88.25%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -4,34 +4,14 @@ public:
         int l =0; 
         int h = arr.size()-1;
         
-        while(l <= h){
+        while(l < h){
             int mid  = (l + h) >> 1;
-            // cout<<mid<<"\n";
-            if(mid == 0 || mid == arr.size()-1){
-                if(mid == 0 && arr[mid] > arr[mid+1]){
-                     return mid;   
-                }
-                if(mid == arr.size()-1 && arr[mid] > arr[mid-1]){
-                    return mid;
-                }
-                if(mid == 0){
-                    l = mid+1;
-                    continue;
-                }else{
-                    h = mid-1;
-                    continue;
-                }
-            }
-            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]){
-                // cout<<arr[mid]<<"\n";
-                return mid;
-            }else if(arr[mid] < arr[mid+1]){
+            if(arr[mid] < arr[mid+1]){
                 l = mid+1;
             }else{
-                h = mid-1;
-                cout<<h<<"\n";
+                h = mid;
             }
         }
-        return 0;
+        return l;
     }
 };
