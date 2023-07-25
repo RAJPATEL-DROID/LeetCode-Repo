bd73d54c34f6ab5196599eee2d348b5fcdb5f3ad
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-07-25 09:22:00 +0530

    Time: 113 ms (92.68%), Space: 59.6 MB (88.25%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int l =0; 
-        int h = arr.size()-1;
+        int l =1; 
+        int h = arr.size()-3;
         
-        while(l < h){
+        while(l <= h){
             int mid  = (l + h) >> 1;
+            // cout<<mid<<"\n";
             if(arr[mid] < arr[mid+1]){
                 l = mid+1;
             }else{
-                h = mid;
+                h = mid-1;
+                // cout<<h<<"\n";
             }
         }
         return l;
