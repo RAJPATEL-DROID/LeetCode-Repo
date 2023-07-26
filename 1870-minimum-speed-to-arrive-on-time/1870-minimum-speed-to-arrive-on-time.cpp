65c5aaaedae477312dff8067dfbb22c2f26391e3
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-07-26 20:21:40 +0530

    Time: 328 ms (64.46%), Space: 101.4 MB (94.10%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -15,6 +15,9 @@ public:
     }
     
     int minSpeedOnTime(vector<int>& dist, double hour) {
+        
+        if(hour <= dist.size()-1)return -1;
+        
         int l = 1;
         int h = 1e7;
         int minSpeed =-1;
