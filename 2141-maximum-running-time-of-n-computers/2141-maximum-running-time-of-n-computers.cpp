13c7f5e75c97a465f1f366a0ea63f2f34a901079
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-07-27 20:04:33 +0530

    Time: 105 ms (100.00%), Space: 55.8 MB (36.33%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -1,11 +1,33 @@
 class Solution {
 public:
-    long long maxRunTime(int n, vector<int>& A) {
-        sort(A.begin(), A.end());
-        long long sum = accumulate(A.begin(), A.end(), 0L);
-        int k = 0, na = A.size();
-        while (A[na - 1 - k] > sum / (n - k))
-            sum -= A[na - 1 - k++];
-        return sum / (n - k);
+    long long maxRunTime(int n, vector<int>& batteries) {
+        // sort(A.begin(), A.end());
+        // long long sum = accumulate(A.begin(), A.end(), 0L);
+        // int k = 0, na = A.size();
+        // while (A[na - 1 - k] > sum / (n - k))
+        //     sum -= A[na - 1 - k++];
+        // return sum / (n - k);
+        
+        long sumPower = 0;
+        for (int power : batteries)
+            sumPower += power;
+        long left = 1, right = sumPower / n;
+        
+        while (left < right){
+            long target = right - (right - left) / 2;
+            long extra = 0;
+            
+            for (int power : batteries){
+                if(power < target)extra += power;
+                else extra += target;
+            }
+            
+
+            if (extra >= (long)(n * target))
+                left = target;
+            else
+                right = target - 1;
+        }
+        return left;
     }
 };
