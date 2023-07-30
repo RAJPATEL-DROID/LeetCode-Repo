da8411ba0e11dfc4ea70fea3eca3609f0610f05f
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-07-30 10:34:02 +0530

    Added solution - LeetHub

--- Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
+++ Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
@@ -5,39 +5,53 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
+    void dfs(int sr,int sc,vector<vector<int>>& image,int oldColor,int newColor,int n,int m){
+        if(sc <0 || sr<0 || sr>=n || sc >= m || image[sr][sc] != oldColor)return;
+        image[sr][sc] = newColor;
+        dfs(sr,sc+1,image,oldColor,newColor,n,m);
+        dfs(sr+1,sc,image,oldColor,newColor,n,m);
+        dfs(sr,sc-1,image,oldColor,newColor,n,m);
+        dfs(sr-1,sc,image,oldColor,newColor,n,m);
+    }
+
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         // Code here 
         int n = image.size();
         int m = image[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
         int ini=image[sr][sc];
-        image[sr][sc] = newColor;
-        
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        
-        vector<int> dir = {-1,0,1,0,-1};
-        
-        vis[sr][sc] = 1;
-        while(!q.empty()){
-            int sz = q.size();
-            while(sz--){
-                pair<int,int> p = q.front();
-                q.pop();
-                
-                for(int i=0;i < 4; i++){
-                    int r = p.first  + dir[i];
-                    int c = p.second + dir[i+1];
-                    
-                    if(r>= 0 && r < n && c>=0 && c< m && image[r][c] == ini && vis[r][c] == 0){
-                        vis[r][c]= 1;
-                        image[r][c] = newColor;
-                        q.push({r,c});
-                    }
-                }
-            }
-        }
+        if(image[sr][sc] == newColor)return image;
+        // DFS
+        dfs(sr,sc,image,ini,newColor,n,m);
         return image;
+        
+        // BFS
+        // image[sr][sc] = newColor;
+        // queue<pair<int,int>>q;
+        // q.push({sr,sc});
+        
+        // vector<vector<int>> vis(n,vector<int>(m,0));
+        // vector<int> dir = {-1,0,1,0,-1};
+        
+        // vis[sr][sc] = 1;
+        // while(!q.empty()){
+        //     int sz = q.size();
+        //     while(sz--){
+        //         pair<int,int> p = q.front();
+        //         q.pop();
+                
+        //         for(int i=0;i < 4; i++){
+        //             int r = p.first  + dir[i];
+        //             int c = p.second + dir[i+1];
+                    
+        //             if(r>= 0 && r < n && c>=0 && c< m && image[r][c] == ini && vis[r][c] == 0){
+        //                 vis[r][c]= 1;
+        //                 image[r][c] = newColor;
+        //                 q.push({r,c});
+        //             }
+        //         }
+        //     }
+        // }
+        // return image;
     }
 };
 
