54241a2c445708caa9dc9c9e541b29bad679ed2d
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-08-10 23:01:46 +0530

    Added solution - LeetHub

--- Bipartite Graph - GFG/bipartite-graph.cpp
+++ Bipartite Graph - GFG/bipartite-graph.cpp
@@ -5,45 +5,76 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
     private:
-    bool check(int start,vector<int> adj[],int color[]){
+    // bool check(int start,vector<int> adj[],int color[]){
         
-        queue<int> q;
-        q.push(start);
-        color[start] =0;
+    //     queue<int> q;
+    //     q.push(start);
+    //     color[start] =0;
         
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            for(auto it : adj[node]){
-                if(color[it] == -1){
-                    color[it] = !color[node];
-                    q.push(it);
-                }else if(color[it] == color[node]){
-                    return false;
-                }
+    //     while(!q.empty()){
+    //         int node = q.front();
+    //         q.pop();
+    //         for(auto it : adj[node]){
+    //             if(color[it] == -1){
+    //                 color[it] = !color[node];
+    //                 q.push(it);
+    //             }else if(color[it] == color[node]){
+    //                 return false;
+    //             }
+    //         }
+    //     }
+        
+    //     return true;
+    // }
+    private:
+    bool dfs(int node,int col,vector<int> adj[],int color[]){
+        color[node] =col;
+        
+        for(int it : adj[node]){
+            if(color[it] == -1){
+                if(dfs(it,!col,adj,color) == false)return false;
+            }else if(color[it] == col){
+               return false;
+                
             }
         }
-        
         return true;
     }
+    
 public:
 	bool isBipartite(int V, vector<int>adj[]){
 	    // Code here
-	    int color[V];
-	    for(int i=0;i<V;i++){
-	        color[i] = -1;
-	    }
+	   
+	   // BFS
+	   
+	   // int color[V];
+	   // for(int i=0;i<V;i++){
+	   //     color[i] = -1;
+	   // }
 	    
-	    for(int i=0; i< V; i++){
+	   // for(int i=0; i< V; i++){
+	   //     if(color[i] == -1){
+	   //         if(check(i,adj,color) == false){
+	   //             return false;
+	   //         }
+	   //     }
+	   // }
+	    
+	   // return true;	
+	    
+	    // DFS
+	    int color[V];
+	    for(int i=0; i < V;i++)color[i] =-1;
+	    
+	    for(int i=0;i< V;i++){
 	        if(color[i] == -1){
-	            if(check(i,adj,color) == false){
+	            if(dfs(i,0,adj,color) == false){
 	                return false;
 	            }
 	        }
 	    }
 	    
-	    return true;	
-	    
+	    return true;
 	}
 
 };
