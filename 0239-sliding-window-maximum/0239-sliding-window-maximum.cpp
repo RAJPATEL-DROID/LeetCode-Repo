4bea0b3f46ec57be4eb6105c45994e55f2cf52b7
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-08-16 23:33:21 +0530

    Time: 198 ms (92.42%), Space: 134.7 MB (66.10%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,22 +1,51 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& a, int k) {
-        vector<int>ans;
-        priority_queue<pair<int,int> >pq;
-        
-        for(int i=0;i<k;i++){
-            pq.push({a[i],i});
+    vector<int> maxSlidingWindow(vector<int>& nums, int k){
+        deque<int> dq;
+        vector<int> res;
+        for (int i = 0; i < k; i++) {
+            while (!dq.empty() && nums[i] >= nums[dq.back()]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
         }
-        ans.push_back(pq.top().first);
-        
-        for(int i=k;i<a.size();i++){
-            pq.push({a[i],i});
-           
-            while(i-pq.top().second>=k)pq.pop();
-           
-            ans.push_back(pq.top().first);
-        }
-        return ans;
+        res.push_back(nums[dq.front()]);
 
+        for (int i = k; i < nums.size(); i++) {
+            if(dq.front() == i - k) {
+                dq.pop_front();
+            }
+            while (!dq.empty() && nums[i] >= nums[dq.back()]) {
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+            res.push_back(nums[dq.front()]);
+        }
+        
+        return res;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& a, int k) {
+//         vector<int>ans;
+//         priority_queue<pair<int,int> >pq;
+        
+//         for(int i=0;i<k;i++){
+//             pq.push({a[i],i});
+//         }
+//         ans.push_back(pq.top().first);
+        
+//         for(int i=k;i<a.size();i++){
+//             pq.push({a[i],i});
+           
+//             while(i-pq.top().second>=k)pq.pop();
+           
+//             ans.push_back(pq.top().first);
+//         }
+//         return ans;
+
+//     }
+// };
