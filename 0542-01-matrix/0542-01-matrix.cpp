aba5c9c06d1dfe903e1b1e0d71df21b22a79ada0
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-08-17 16:34:16 +0530

    Time: 72 ms (77.00%), Space: 34.4 MB (42.54%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,37 +3,39 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
+        
         vector<vector<int>> dis(n,vector<int>(m,0));
         vector<vector<int>> vis(n,vector<int>(m,0));
+        
         queue<pair<pair<int,int>,int>> q;
-        for(int i=0;i <n; i++){
-            for(int j=0; j< m ; j++){
+        
+        for(int i=0;i < n;i++){
+            for(int j=0;j<m;j++){
                 if(mat[i][j] == 0){
-                    q.push({{i,j},0});
                     vis[i][j] = 1;
+                    q.push({{i,j},0});
                 }
             }
         }
-        vector<int> dir = {-1,0,1,0,-1};
+        int dir[5]={-1,0,1,0,-1};
         while(!q.empty()){
             int row = q.front().first.first;
             int col = q.front().first.second;
-            int step = q.front().second;
-            dis[row][col] = step;
+            int curr_dist = q.front().second;
+            dis[row][col] = curr_dist;
             q.pop();
-            
-            for(int i=0;i< 4; i++){
-                int x = row + dir[i];
-                int y = col + dir[i+1];
+            for(int i=0;i < 4;i++){
+                int r = row + dir[i];
+                int c = col + dir[i+1];
                 
-                if(x>=0 && x < n && y >=0 && y < m && 
-                  vis[x][y] == 0){
-                    vis[x][y] =1;
-                    q.push({{x,y},step+1});
-                }
+                if( r >= 0 && c >= 0 && r < n && c < m && vis[r][c] == 0 ){
+                    vis[r][c] =1;
+                    q.push({{r,c},curr_dist + 1});
+                } 
             }
         }
         
         return dis;
+        
     }
 };
