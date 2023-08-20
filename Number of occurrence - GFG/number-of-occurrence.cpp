8f5414f7d98e8bd496db703618ac2dff5cd1dc11
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-08-20 14:59:15 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -15,39 +15,40 @@ public:
 	    
 	    int l = 0;
 	    int r= n-1;
-	    int idx = -1;
+	    int lb =0;
+	    while(l <= r){
+	        int mid = (l + r) >> 1;
+	        if(arr[mid] == x){
+	            lb = mid;
+	            r = mid-1;
+	        }else if(arr[mid] > x){
+	            r = mid -1;
+	        }else{
+	            l = mid + 1;
+	        }
+	    }
+	    
+	    int rb =0;
+	    l =0;
+	    r = n-1;
 	    
 	    while(l <= r){
 	        int mid = (l + r) >> 1;
 	        if(arr[mid] == x){
-	            idx = mid;
-	            break;
+	            rb = mid;
+	            l = mid +1;
 	        }else if(arr[mid] > x){
-	            r = mid-1;
-	        }else if(arr[mid] < x){
-	            l = mid+1;
+	            r = mid -1;
+	        }else{
+	            l = mid + 1;
 	        }
 	    }
-	    
-	    if(idx == -1){
+	    rb++;
+	    if(lb != n && arr[lb] != x){
 	        return 0;
 	    }
-	    
-	    int count =1;
-	    int k = idx -1;
-	    
-	    while(arr[k] == x && k >-1){
-	        count++;
-	        k--;
-	    }
-	    
-	    k = idx+1;
-	    while(arr[k] == x && k < n){
-	        count++;
-	        k++;
-	    }
-	    
-	    return count;
+	    return rb - lb;
+	   
 	}
 };
 
