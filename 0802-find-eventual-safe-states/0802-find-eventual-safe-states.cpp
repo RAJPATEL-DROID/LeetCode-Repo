2c2667fcd8d43f3aa875321c0c6ca76d02a445e5
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-08-22 23:24:13 +0530

    Time: 145 ms (94.74%), Space: 61.6 MB (22.62%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,92 +1,92 @@
 // TOPOLOGICAL SORT
-// class Solution {
-// public:
-//     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-//         int n = graph.size();
-//         vector<int> indegree(n);
-//         vector<vector<int>> adj(n);
-
-//         for (int i = 0; i < n; i++) {
-//             for (auto node : graph[i]) {
-//                 adj[node].push_back(i);
-//                 indegree[i]++;
-//             }
-//         }
-
-//         queue<int> q;
-//         // Push all the nodes with indegree zero in the queue.
-//         for (int i = 0; i < n; i++) {
-//             if (indegree[i] == 0) {
-//                 q.push(i);
-//             }
-//         }
-
-//         vector<bool> safe(n);
-//         while (!q.empty()) {
-//             int node = q.front();
-//             q.pop();
-//             safe[node] = true;
-
-//             for (auto& neighbor : adj[node]) {
-//                 // Delete the edge "node -> neighbor".
-//                 indegree[neighbor]--;
-//                 if (indegree[neighbor] == 0) {
-//                     q.push(neighbor);
-//                 }
-//             }
-//         }
-
-//         vector<int> safeNodes;
-//         for(int i = 0; i < n; i++) {
-//             if(safe[i]) {
-//                 safeNodes.push_back(i);
-//             }
-//         }
-//         return safeNodes;
-//     }
-// };
-
-// DFS Solution
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>& adj, vector<bool>& visit, vector<bool>& pathVisit) {
-        // Mark the current node as visited and part of current recursion stack.
-        visit[node] = true;
-        pathVisit[node] = true;
-        for (auto neighbor : adj[node]) {
-            if(!visit[neighbor]){
-                if (dfs(neighbor, adj, visit, pathVisit)) {
-                    return true;
-                }
-            }else if(pathVisit[neighbor])return true;
-        }
-        // Remove the node from the stack.
-        pathVisit[node] = false;
-        return false;
-    }
-
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
+        vector<int> indegree(n);
         vector<vector<int>> adj(n);
 
         for (int i = 0; i < n; i++) {
             for (auto node : graph[i]) {
-                adj[i].push_back(node);
+                adj[node].push_back(i);
+                indegree[i]++;
             }
         }
 
-        vector<bool> visit(n), pathVisit(n);
-
+        queue<int> q;
+        // Push all the nodes with indegree zero in the queue.
         for (int i = 0; i < n; i++) {
-            dfs(i, adj, visit, pathVisit);
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        vector<bool> safe(n);
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            safe[node] = true;
+
+            for (auto& neighbor : adj[node]) {
+                // Delete the edge "node -> neighbor".
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.push(neighbor);
+                }
+            }
         }
 
         vector<int> safeNodes;
-        for (int i = 0; i < n; i++) {
-            if (!pathVisit[i]) {
+        for(int i = 0; i < n; i++) {
+            if(safe[i]) {
                 safeNodes.push_back(i);
             }
         }
         return safeNodes;
     }
 };
+
+// DFS Solution
+// class Solution {
+// public:
+//     bool dfs(int node, vector<vector<int>>& adj, vector<bool>& visit, vector<bool>& pathVisit) {
+//         // Mark the current node as visited and part of current recursion stack.
+//         visit[node] = true;
+//         pathVisit[node] = true;
+//         for (auto neighbor : adj[node]) {
+//             if(!visit[neighbor]){
+//                 if (dfs(neighbor, adj, visit, pathVisit)) {
+//                     return true;
+//                 }
+//             }else if(pathVisit[neighbor])return true;
+//         }
+//         // Remove the node from the stack.
+//         pathVisit[node] = false;
+//         return false;
+//     }
+
+//     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+//         int n = graph.size();
+//         vector<vector<int>> adj(n);
+
+//         for (int i = 0; i < n; i++) {
+//             for (auto node : graph[i]) {
+//                 adj[i].push_back(node);
+//             }
+//         }
+
+//         vector<bool> visit(n), pathVisit(n);
+
+//         for (int i = 0; i < n; i++) {
+//             dfs(i, adj, visit, pathVisit);
+//         }
+
+//         vector<int> safeNodes;
+//         for (int i = 0; i < n; i++) {
+//             if (!pathVisit[i]) {
+//                 safeNodes.push_back(i);
+//             }
+//         }
+//         return safeNodes;
+//     }
+// };
