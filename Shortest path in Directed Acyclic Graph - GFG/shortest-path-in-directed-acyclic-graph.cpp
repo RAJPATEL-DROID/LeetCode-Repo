4a1b3c7af613c2c0d8f65d1bc7b9479154674773
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-08-25 00:04:09 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -8,34 +8,75 @@ using namespace std;
 // User function Template for C++
 class Solution {
   public:
+    void topoSort(int i,vector<pair<int,int> > adj[],stack<int>& s,vector<int>& vis){
+        vis[i] =1;
+        for(auto it : adj[i]){
+            if(!vis[it.first]){
+                topoSort(it.first,adj,s,vis);
+            }
+        }
+        s.push(i);
+    };
      vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
         // code here
-        vector<vector<int>> adj[N];
+        
+        // BFS
+        vector<pair<int,int> > adj[N];
         for(auto e : edges)
             adj[e[0]].push_back( {e[1], e[2]} );
 
-        vector<int> distance(N, INT_MAX);
-        distance[0] = 0;
-        queue<int> q;
-        q.push(0);
+        // vector<int> distance(N, INT_MAX);
+        // distance[0] = 0;
+    //     queue<int> q;
+    //     q.push(0);
         
-        while(q.size()){
-            int node = q.front();   q.pop(); 
+    //     while(q.size()){
+    //         int node = q.front();   q.pop(); 
 
-            for(auto padosi : adj[node]){
-                int nbr = padosi[0] ;
-                int dist = padosi[1];
-                if(distance[node] + dist < distance[nbr]){
-                    distance[nbr] = distance[node] + dist ;
-                    q.push(nbr);
+    //         for(auto padosi : adj[node]){
+    //             int nbr = padosi[0] ;
+    //             int dist = padosi[1];
+    //             if(distance[node] + dist < distance[nbr]){
+    //                 distance[nbr] = distance[node] + dist ;
+    //                 q.push(nbr);
+    //             }
+    //         }
+    //     }
+    //     for(auto &a : distance)
+    //         if(a==INT_MAX)a = -1;
+    //     return distance;
+    // }
+    
+        vector<int> vis(N,0);
+        stack<int> s;
+        for(int i=0;i < N; i++){
+            if(!vis[i]){
+                topoSort(i,adj,s,vis);
+            }
+        }
+        
+        // Relax the edges
+        vector<int> dis(N,1e9);
+        dis[0] =0;
+        while(!s.empty()){
+            int node = s.top();
+            s.pop();
+            
+            for(auto it : adj[node]){
+                int v = it.first;
+                int wt = it.second;
+                if(dis[v]  > dis[node] + wt){
+                    dis[v] = dis[node] + wt;
                 }
             }
         }
-        for(auto &a : distance)
-            if(a==INT_MAX)a = -1;
-        return distance ;
         
-    }
+        for(int i=0; i< N; i++){
+            if(dis[i] == 1e9)dis[i] = -1;
+        }
+    
+        return dis;
+     }
 };
 
 
