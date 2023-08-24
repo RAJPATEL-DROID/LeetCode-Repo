53ec0a77c53502fb11ae29af0c72e1baad292855
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-08-24 23:11:09 +0530

    Added solution - LeetHub

--- Shortest path in Undirected Graph having unit distance - GFG/shortest-path-in-undirected-graph-having-unit-distance.cpp
+++ Shortest path in Undirected Graph having unit distance - GFG/shortest-path-in-undirected-graph-having-unit-distance.cpp
@@ -10,28 +10,31 @@ class Solution {
   public:
     vector<int> shortestPath(vector<vector<int>>& edges, int N,int M, int src){
         // code here
-        vector<vector<int>>adjList(N);
-        for(int i=0; i < M; i++){
-            adjList[edges[i][0]].push_back(edges[i][1]);
-            adjList[edges[i][1]].push_back(edges[i][0]);
-            
+        vector<int> adj[N];
+        
+        for(auto it : edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
-        
-        vector<int> dis(N,-1);
+        vector<int> dis(N);
+        for(int i=0;i<N;i++)dis[i] = 1e9;
         dis[src] = 0;
-        
         queue<int> q;
         q.push(src);
         
         while(!q.empty()){
             int node = q.front();
             q.pop();
-            for(auto it : adjList[node]){
-                if(dis[it] == -1){
-                    dis[it] = dis[node] + 1;
+            
+            for(auto it : adj[node]){
+                if(dis[it] > dis[node] + 1){
                     q.push(it);
-                } 
+                    dis[it] = dis[node] + 1;
+                }
             }
+        }
+        for(int i=0;i<N;i++){
+            if(dis[i] == 1e9)dis[i] = -1;
         }
         return dis;
     }
