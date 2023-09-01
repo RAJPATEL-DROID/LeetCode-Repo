e3a2fd976198abf9a18c7c9e532cd4768ad06440
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-09-01 22:54:54 +0530

    Added solution - LeetHub

--- Shortest Path in Weighted undirected graph - GFG/shortest-path-in-weighted-undirected-graph.cpp
+++ Shortest Path in Weighted undirected graph - GFG/shortest-path-in-weighted-undirected-graph.cpp
@@ -9,49 +9,51 @@ class Solution {
         // Code here
         vector<pair<int,int>> adj[n+1];
         
-        for(auto it:edges){
-             adj[it[0]].push_back({it[1],it[2]});
-             adj[it[1]].push_back({it[0],it[2]});
+        for(auto it: edges){
+            adj[it[0]].push_back({it[1],it[2]});
+            adj[it[1]].push_back({it[0],it[2]});
         }
         
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int> > > pq;
-        
-        pq.push({0,1});
-        
-        vector<int> dist(n+1,INT_MAX);
-        
+        vector<int> dist(n+1,1e9), parent(n+1);
+        for(int i=1;i<=n;i++)parent[i] = i;
         dist[1] =0;
         
-        vector<int> parent(n+1,-1);
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        
+        pq.push({dist[1],1});
         
         while(!pq.empty()){
-            int d = pq.top().first;
-            int node = pq.top().second;
+            auto it = pq.top();
+            int node = it.second;
+            int dis = it.first;
             pq.pop();
-            
-            if(d > dist[node])continue;
+        
             for(auto it: adj[node]){
+                int adjWeight = it.second;
                 int adjNode = it.first;
-                int wt = it.second;
-                if(dist[adjNode] > d + wt){
-                    dist[adjNode] = d + wt;
+                
+                if(dis + adjWeight < dist[adjNode]){
+                    dist[adjNode]  = dis + adjWeight;
                     parent[adjNode] = node;
-                    pq.push({d + wt, adjNode});
+                    pq.push({dis + adjWeight,adjNode});
                 }
             }
-        }
-        if(dist[n] == INT_MAX)return {-1};
+        } 
+        
+        
+        if(dist[n] == 1e9)return {-1};
         
         vector<int> ans;
-        while(n != -1){
-            ans.push_back(n);
-            n = parent[n];
+        int node =n;
+        
+        while(parent[node] != node){
+            ans.push_back(node);
+            node = parent[node];
         }
         
+        ans.push_back(1);
         reverse(ans.begin(),ans.end());
         return ans;
-    
-        
     }
 };
 
