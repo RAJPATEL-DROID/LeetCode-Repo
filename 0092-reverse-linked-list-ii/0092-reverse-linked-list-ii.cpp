eca3cbc1d1b82176c173ff51a14abb99465c0516
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-09-07 11:30:12 +0530

    Time: 2 ms (52.03%), Space: 7.5 MB (46.43%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -11,23 +11,41 @@
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        ListNode* dummy = new ListNode(0);
+//         ListNode dummy(0);  // this will get us memory on stack so no worry about memory Leak
+//         dummy.next = head;
+        
+//         ListNode* prev = &dummy;
+//         ListNode* curr = NULL;
+//         for(int i=0; i < left -1;i++){
+//             prev = prev->next;
+//         }
+//         curr= prev->next;
+        
+//         for(int i=0; i < right-left;i++){
+//             ListNode* frwd = curr->next;
+//             curr->next  = frwd->next;
+//             frwd->next = prev->next;
+//             prev->next = frwd;
+//         }
+        
+//         return dummy.next;
+        
+        ListNode* dummy = new ListNode(NULL);
         ListNode* prev = dummy;
         ListNode* curr = NULL;
         dummy->next = head;
-        for(int i=0; i < left -1;i++){
+        for(int i=0;i < left-1;i++){
             prev = prev->next;
         }
-        curr= prev->next;
+        curr = prev->next;
         
-        for(int i=0; i < right-left;i++){
+        for(int i=0;i <right-left; i++){
             ListNode* frwd = curr->next;
-            curr->next  = frwd->next;
+            curr->next = frwd->next;
             frwd->next = prev->next;
             prev->next = frwd;
         }
         
         return dummy->next;
-        
     }
 };
