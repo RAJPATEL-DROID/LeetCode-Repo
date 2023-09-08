b65e812361be054d50b12085be5810879f4e8063
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-09-08 22:45:46 +0530

    Added solution - LeetHub

--- City With the Smallest Number of Neighbors at a Threshold Distance - GFG/city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ City With the Smallest Number of Neighbors at a Threshold Distance - GFG/city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -11,40 +11,86 @@ class Solution {
     int findCity(int n, int m, vector<vector<int>>& edges,
                  int distanceThreshold) {
                      
-        vector<vector<int>> matrix(n,vector<int>(n,INT_MAX));
+        // vector<vector<int>> matrix(n,vector<int>(n,INT_MAX));
 	    
-	    for(auto it : edges){
-            matrix[it[0]][it[1]] = it[2];
-            matrix[it[1]][it[0]] = it[2];
-        }
-	    for(int i=0;i < n;i++)matrix[i][i] =0;
+	   // for(auto it : edges){
+    //         matrix[it[0]][it[1]] = it[2];
+    //         matrix[it[1]][it[0]] = it[2];
+    //     }
+	   // for(int i=0;i < n;i++)matrix[i][i] =0;
         
-	    for(int k=0; k < n;k++){
-	        for(int i=0;i < n;i++){
-	            for(int j=0;j < n; j++){
-	                if(matrix[i][k] == INT_MAX || matrix[k][j] == INT_MAX)continue;
-	                matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
-	            }
-	        }
-	    }
+	   // for(int k=0; k < n;k++){
+	   //     for(int i=0;i < n;i++){
+	   //         for(int j=0;j < n; j++){
+	   //             if(matrix[i][k] == INT_MAX || matrix[k][j] == INT_MAX)continue;
+	   //             matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+	   //         }
+	   //     }
+	   // }
         
         int cityCnt = n;
         int city = -1;
-        for(int i=0;i <n;i++){
+    //     for(int i=0;i <n;i++){
+    //         int cnt =0;
+    //         for(int j=0;j<n;j++){
+    //             if(matrix[i][j] <= distanceThreshold)cnt++;
+    //         }
+            
+    //         if(cnt <= cityCnt){
+    //             city = i;
+    //             cityCnt = cnt;
+    //         }
+    //     }
+        
+    //     return city;
+               
+       vector<pair<int,int> > adj[n];
+      
+        for(auto it : edges){
+           adj[it[0]].push_back({it[1],it[2]});
+           adj[it[1]].push_back({it[0],it[2]});
+        }
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+        
+        for(int k=0; k < n; k++){
+            vector<int> dist(n,1e9);
+            dist[k] =0;
+            pq.push({0,k});
+            
+            while(!pq.empty())
+            {
+                auto it = pq.top();
+                pq.pop();
+                
+                int city =it.second;
+                int wt = it.first;
+                
+                for(auto itr: adj[city]){
+                    int adjCity = itr.first;
+                    int adjWt = itr.second;
+                    
+                    if(wt + adjWt < dist[adjCity]){
+                        dist[adjCity]  = wt + adjWt;
+                        pq.push({dist[adjCity],adjCity});
+                    }
+                }
+            }
+            
             int cnt =0;
-            for(int j=0;j<n;j++){
-                if(matrix[i][j] <= distanceThreshold)cnt++;
+            
+            for(int i=0; i < n; i++){
+                if(dist[i] <= distanceThreshold)cnt++;
             }
             
             if(cnt <= cityCnt){
-                city = i;
                 cityCnt = cnt;
+                city = k;
             }
         }
         
         return city;
-                     
-                     
+       
     }
 };
 
