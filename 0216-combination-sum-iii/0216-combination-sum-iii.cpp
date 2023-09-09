b2ad206a42dfa6a324283f7166ba49b46909066a
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-09-09 20:11:07 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (28.42%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -26,6 +26,19 @@ public:
         GenerateSum(i+1,k,n,ans,temp);
         temp.pop_back();
         
+//         if(!k || !n){
+//             if(!k && !n){ // Means we got our sum
+//                 ans.push_back(temp);
+//             }
+//             return;
+//         }
+        
+//         for(int j=i ;j <= min(9,n); j++){
+//             temp.push_back(j);
+//             GenerateSum(j+1,k-1,n-j,ans,temp);
+//             temp.pop_back();
+//         }
+        
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int> > ans;
