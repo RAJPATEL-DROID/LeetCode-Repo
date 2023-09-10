adb6f06e9f7c68549702b390eef309d51c5ad996
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-09-10 19:11:27 +0530

    Time: 2 ms (56.67%), Space: 6 MB (71.30%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,11 +1,15 @@
 class Solution {
 const int MOD = 1e9 + 7;
 public:
-    int countOrders(int n) {
-        long long count = 1;
-        for (int i = 2; i <= n; ++i) {
-            count = (count * (2 * i - 1) * i) % MOD;
-        }
-        return (int)count;
+    // int countOrders(int n) {
+    //     long long count = 1;
+    //     for (int i = 2; i <= n; ++i) {
+    //         count = (count * (2 * i - 1) * i) % MOD;
+    //     }
+    //     return (int)count;
+    // }
+     int countOrders(int n, long res = 1) {
+        return n ? countOrders(n - 1, res * (n * 2 - 1) * n % long(1e9+7)) : res;
     }
+
 };
