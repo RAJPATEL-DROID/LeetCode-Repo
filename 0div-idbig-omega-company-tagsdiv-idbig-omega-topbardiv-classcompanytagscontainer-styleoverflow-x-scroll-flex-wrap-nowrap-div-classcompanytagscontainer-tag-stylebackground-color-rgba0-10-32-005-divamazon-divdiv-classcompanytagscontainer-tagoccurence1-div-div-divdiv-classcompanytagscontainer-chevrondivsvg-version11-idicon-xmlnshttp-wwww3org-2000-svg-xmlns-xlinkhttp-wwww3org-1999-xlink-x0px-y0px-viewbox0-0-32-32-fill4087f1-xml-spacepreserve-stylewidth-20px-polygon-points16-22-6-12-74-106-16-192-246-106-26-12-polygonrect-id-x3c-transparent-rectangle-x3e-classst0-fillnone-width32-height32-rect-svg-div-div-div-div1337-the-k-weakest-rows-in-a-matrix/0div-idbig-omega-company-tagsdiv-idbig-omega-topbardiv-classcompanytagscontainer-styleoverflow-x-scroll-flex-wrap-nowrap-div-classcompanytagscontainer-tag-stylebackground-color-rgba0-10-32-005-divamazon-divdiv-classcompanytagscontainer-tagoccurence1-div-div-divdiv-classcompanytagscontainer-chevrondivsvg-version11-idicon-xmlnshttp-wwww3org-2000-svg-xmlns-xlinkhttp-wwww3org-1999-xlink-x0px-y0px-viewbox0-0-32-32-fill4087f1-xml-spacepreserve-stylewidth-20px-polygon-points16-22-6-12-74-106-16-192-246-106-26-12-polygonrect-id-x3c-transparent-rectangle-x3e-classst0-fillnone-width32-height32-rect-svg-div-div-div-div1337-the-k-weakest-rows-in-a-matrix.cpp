285786ecc7c8aa7d7965763ae73ca2f751d87251
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-09-18 12:00:48 +0530

    Time: 13 ms (31.65%), Space: 10.7 MB (13.97%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1337-the-k-weakest-rows-in-a-matrix/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1337-the-k-weakest-rows-in-a-matrix/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        int m = mat.size();
-        int n = mat[0].size();
-        vector<int> numSol(m);
-        for(int i=0; i< m; i++){
-            int sold =0;
-            for(int j=0; j < n;j++){
-                if(mat[i][j] == 1)sold++;
-                if(mat[i][j] == 0)break;
-            }
-            numSol[i] = sold;
-        }
+//         int m = mat.size();
+//         int n = mat[0].size();
+//         vector<int> numSol(m);
+//         for(int i=0; i< m; i++){
+//             int sold =0;
+//             for(int j=0; j < n;j++){
+//                 if(mat[i][j] == 1)sold++;
+//                 if(mat[i][j] == 0)break;
+//             }
+//             numSol[i] = sold;
+//         }
         
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+//         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
         
-        for(int i=0;i<m;i++){
-            pq.push({numSol[i],i});
+//         for(int i=0;i<m;i++){
+//             pq.push({numSol[i],i});
+//         }
+//         vector<int> ans;
+//         while(k){
+//             ans.push_back(pq.top().second);
+//             pq.pop();
+//             k--;
+//         }
+//         return ans;
+        
+        int n=mat[0].size();
+        for(int i=0; i<mat.size(); i++){
+            mat[i].push_back(i);
         }
-        vector<int> ans;
-        while(k){
-            ans.push_back(pq.top().second);
-            pq.pop();
-            k--;
+        sort(mat.begin(), mat.end());
+        vector<int> ans(k);
+        for(int i=0; i<k; i++){
+            ans[i]=mat[i][n];            
         }
         return ans;
     }
