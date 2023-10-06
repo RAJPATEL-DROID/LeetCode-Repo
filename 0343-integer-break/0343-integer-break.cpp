735ce122f84790eb8be7b295033aab1ba97ac644
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-10-06 18:52:13 +0530

    Time: 2 ms (49.96%), Space: 6.5 MB (29.03%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,16 +1,47 @@
+// class Solution {
+// public:
+//     int integerBreak(int n) {
+//         if (n <= 3) {
+//             return n - 1;
+//         }
+        
+//         int ans = 1;
+//         while (n > 4) {
+//             ans *= 3;
+//             n -= 3;
+//         }
+        
+//         return ans * n;
+//     }
+// };
 class Solution {
 public:
+    vector<int> memo;
+    
     int integerBreak(int n) {
         if (n <= 3) {
             return n - 1;
         }
         
-        int ans = 1;
-        while (n > 4) {
-            ans *= 3;
-            n -= 3;
+        memo = vector(n + 1, 0);
+        return dp(n);
+    }
+    
+    int dp(int num) {
+        if (num <= 3) {
+            return num;
         }
         
-        return ans * n;
+        if (memo[num] != 0) {
+            return memo[num];
+        }
+        
+        int ans = num;
+        for (int i = 2; i < num; i++) {
+            ans = max(ans, i * dp(num - i));
+        }
+        
+        memo[num] = ans;
+        return ans;
     }
 };
