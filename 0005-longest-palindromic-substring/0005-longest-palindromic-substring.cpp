55445eb6beee8a2ac0d111e0c0a4aeec2abef660
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-10-27 21:53:07 +0530

    Time: 819 ms (18.96%), Space: 23.3 MB (36.13%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,26 +1,50 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int maxLen =0,len =0,start =0;
-        int n =  s.length();
+        // int maxLen =0,len =0,start =0;
+        // int n =  s.length();
         
-        for(int i =0 ;i < n; i++){
-            int l =i-1, r = i+1;
+        // for(int i =0 ;i < n; i++){
+        //     int l =i-1, r = i+1;
             
-            while(l>=0 && s[i] == s[l])l--;
-            while(r < n && s[i] == s[r])r++;
+        //     while(l>=0 && s[i] == s[l])l--;
+        //     while(r < n && s[i] == s[r])r++;
             
-            while(l>=0 && r < n && s[l] == s[r]){
-                l--;r++;
-            }
+        //     while(l>=0 && r < n && s[l] == s[r]){
+        //         l--;r++;
+        //     }
             
-            len = r-l-1;
+        //     len = r-l-1;
             
-            if(len > maxLen){
-                start = l+1;
-                maxLen = len;
+        //     if(len > maxLen){
+        //         start = l+1;
+        //         maxLen = len;
+        //     }
+        // }
+        // return s.substr(start,maxLen);
+
+        int n = s.size();
+        int start = 0,maxLen =0;
+        vector<vector<bool>> dp(n,vector<bool>(n,false));
+
+        for(int len =0; len < n;len++){
+            for(int i=0, j = len; j < n; j++,i++){
+                if(isPalindrome(dp,s,i,j) && len >= maxLen){
+                    start = i;
+                    maxLen = len + 1;
+                }
             }
         }
         return s.substr(start,maxLen);
+    }
+
+    bool isPalindrome(vector<vector<bool>>& dp,string& s,int i,int j){
+        if(i == j){
+            return dp[i][j] = true;
+        }else if(j - i == 1){
+            return dp[i][j] = s[i]==s[j];
+        }else{
+            return dp[i][j] = dp[i+1][j-1] ? s[i]==s[j]:false;
+        }
     }
 };
