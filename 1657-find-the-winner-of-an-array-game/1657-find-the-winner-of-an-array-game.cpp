a37597c1011d6b4a9776f397dbff502201393fcf
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-11-05 19:27:32 +0530

    Time: 92 ms (41.2%), Space: 63.4 MB (86.11%) - LeetHub

--- 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.cpp
+++ 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.cpp
@@ -1,39 +1,60 @@
 class Solution {
 public:
     int getWinner(vector<int>& arr, int k) {
-        int winner =arr[0];
-        int cnt =0;
-        queue<int> q;
-        int n = arr.size();
-        int i=1;
-        while(i < n){
-            if(arr[i] < winner){
-                cnt++;
-                if(cnt == k)return winner;
-                q.push(arr[i]);
-            }else{
-                cnt =1;
-                q.push(winner);
-                winner = arr[i];
-                if(cnt == k)return winner;
-            }
-            i++;
-        }
+        // int winner =arr[0];
+        // int cnt =0;
+        // queue<int> q;
+        // int n = arr.size();
+        // int i=1;
+        // while(i < n){
+        //     if(arr[i] < winner){
+        //         cnt++;
+        //         if(cnt == k)return winner;
+        //         q.push(arr[i]);
+        //     }else{
+        //         cnt =1;
+        //         q.push(winner);
+        //         winner = arr[i];
+        //         if(cnt == k)return winner;
+        //     }
+        //     i++;
+        // }
 
-        if(k > n)return winner;
-        while(!q.empty()){
-            int temp = q.front();
-            if(temp < winner){
+        // if(k > n)return winner;
+        // while(!q.empty()){
+        //     int temp = q.front();
+        //     if(temp < winner){
+        //         cnt++;
+        //         if(cnt == k)return winner;
+        //         q.push(temp);
+        //     }else{
+        //         cnt =1;
+        //         q.push(winner);
+        //         winner = temp;
+        //     }
+        //     q.pop();
+        // }
+
+        // int maxEle = arr[0];
+        // for(int i=1; i< arr.size(); i++){
+        //     maxEle = max(maxEle,arr[i]);
+        // }
+
+        int curr= arr[0];
+        int cnt =0;
+
+        for(int i=1; i< arr.size(); i++){
+            int opponent = arr[i];
+            if(curr > opponent){
                 cnt++;
-                if(cnt == k)return winner;
-                q.push(temp);
             }else{
+                curr = opponent;
                 cnt =1;
-                q.push(winner);
-                winner = temp;
             }
-            q.pop();
+            if(cnt == k ){
+                return curr;
+            }
         }
-        return 1;
+        return curr;
     }
 };
