c0990660b2e93d5ac23761a4aad5893d2c1f4724
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2023-11-05 19:22:17 +0530

    Time: 108 ms (13.89%), Space: 66.3 MB (25.93%) - LeetHub

---
+++ 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int getWinner(vector<int>& arr, int k) {
+        int winner =arr[0];
+        int cnt =0;
+        queue<int> q;
+        int n = arr.size();
+        int i=1;
+        while(i < n){
+            if(arr[i] < winner){
+                cnt++;
+                if(cnt == k)return winner;
+                q.push(arr[i]);
+            }else{
+                cnt =1;
+                q.push(winner);
+                winner = arr[i];
+                if(cnt == k)return winner;
+            }
+            i++;
+        }
+
+        if(k > n)return winner;
+        while(!q.empty()){
+            int temp = q.front();
+            if(temp < winner){
+                cnt++;
+                if(cnt == k)return winner;
+                q.push(temp);
+            }else{
+                cnt =1;
+                q.push(winner);
+                winner = temp;
+            }
+            q.pop();
+        }
+        return 1;
+    }
+};
