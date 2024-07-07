60dfaf2b83ac4794f58466c54865dca62c19ed7c
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2024-07-07 14:54:54 +0530

    Time: 0 ms (100%), Space: 6.9 MB (95.58%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
         int cnt = numBottles;
-        // int currBottles = 0;
         int emptyBottles = numBottles;
         while(emptyBottles >= numExchange){
             cnt += emptyBottles/numExchange;
