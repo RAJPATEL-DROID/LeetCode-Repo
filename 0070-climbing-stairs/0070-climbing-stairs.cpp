039dd7167435f1d530bd90e7d46bd4ecb3781eb6
RAJ PATEL <72067057+RAJPATEL-DROID@users.noreply.github.com>
2025-01-07 10:55:19 +0530

    Time: 0 ms (100%), Space: 7.9 MB (50.6%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-    int dp[46];
-    int solve(int n){
+    int solve(int n,vector<int>& dp){
+
         if(n == 0)return 1;
-        if(n == 1)return 1;
+        if(n < 0)return 0;
 
-        if(dp[n] != -1)return dp[n];
-        int onlyOne = solve(n-1);
-        int twoStair = solve(n-2);
+        if(dp[n] != - 1){
+            return dp[n];
+        }
 
-        return dp[n] = onlyOne + twoStair;
+        return dp[n] = solve(n-1,dp) + solve(n-2,dp);
     }
-    
     int climbStairs(int n) {
-        memset(dp,-1,sizeof(dp));
-        dp[0] =1;
-        dp[1] =0;
-        return solve(n);
+        // vector<int> dp(n+1,-1);
+        int prev1 = 1;
+        int prev2 = 1;
+
+        // solve(n,dp);
+        // return dp[n];
+
+        for(int i=2; i <= n;i++){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }    
+
+        return prev1;
+
     }
 };
